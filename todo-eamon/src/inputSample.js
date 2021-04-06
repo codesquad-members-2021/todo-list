9b1a992d3407a59a5a75eba1409b21032a97c57b
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-04-07 04:16:32 +0900

    [Feat]1-9 여러개의 input 상태 관리 하기

--- todo-eamon/src/inputSample.js
+++ todo-eamon/src/inputSample.js
@@ -1,22 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function InputSample() {
-  const [text, setText] = useState('');
+  const [inputs, setInputs] = useState({
+    name: "",
+    nickname: "",
+    num: "",
+  });
+
+  const { name, nickname, num } = inputs; // 비구조화 할당을 통해 값 추출
 
   const onChange = (e) => {
-    setText(e.target.value);
+    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+    console.log(inputs);
+    setInputs({
+      ...inputs, // 기존의 input 객체를 복사한 뒤
+      [name]: value, // name 키를 가진 값을 value 로 설정
+    });
   };
 
   const onReset = () => {
-    setText('');
+    setInputs({
+      name: "",
+      nickname: "",
+      num: "",
+    });
   };
 
   return (
     <div>
-      <input onChange={onChange} value={text}  />
+      <input name="name" placeholder="이름" onChange={onChange} value={name} />
+      <input
+        name="nickname"
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickname}
+      />
+      <input name="num" placeholder="num" onChange={onChange} value={num} />
       <button onClick={onReset}>초기화</button>
       <div>
-        <b>값: {text}</b>
+        <b>값: </b>
+        {name} ({nickname}) , {num}
       </div>
     </div>
   );
