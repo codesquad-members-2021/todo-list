4764687ac49c64a084cb62dd6c0bbfdbf37a5638
okstring <milkoi7801@gmail.com>
2021-04-06 15:05:50 +0900

    Init TODOList iOS project

---
+++ TODOList/TODOList/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  TODOList
+//
+//  Created by Issac on 2021/04/06.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
