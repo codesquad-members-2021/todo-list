780dbcf98b356a73db8d7769120b228d2ec63073
okstring <milkoi7801@gmail.com>
2021-04-06 15:05:50 +0900

    Init TODOList iOS project

---
+++ TODOList/TODOList/SceneDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.swift
+//  TODOList
+//
+//  Created by Issac on 2021/04/06.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
---
+++ TODOList/TODOList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TODOList
+//
+//  Created by Issac on 2021/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
