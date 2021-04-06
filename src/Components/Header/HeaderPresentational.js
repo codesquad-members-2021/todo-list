a9ba0a4aecc84983a9c14edfae391b778264dd6a
dudn1933 <dudn1933@naver.com>
2021-04-06 23:29:17 +0900

    Make NAVIGATION ON && OFF

--- src/Components/Header/HeaderPresentational.js
+++ src/Components/Header/HeaderPresentational.js
@@ -1,23 +1,69 @@
-import React from 'react'
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
 
 function HeaderPresentational() {
+  const [mode, setMode] = useState(false);
+
   return (
-    <>
-      <CustomSection>
-        나는 프레젠
-      </CustomSection>
-      <CustomSection>
-        나는 프레젠
-      </CustomSection>
-    </>
-  )
+    <div>
+      <Header>
+        <div>Todo-List</div>
+        <Header_button
+          type="button"
+          value="◀"
+          onClick={() => {
+            mode === false ? setMode(true) : setMode(false);
+          }}
+        />
+      </Header>
+      {!mode ? null : (
+        <NAVIGATION>
+          <CLOSE
+            onClick={() => {
+              mode ? setMode(false) : setMode(true);
+            }}
+          >
+            ❌
+          </CLOSE>
+        </NAVIGATION>
+      )}
+    </div>
+  );
 }
 
-const CustomSection = styled.section`
-  background-color: yellow;
+const Header = styled.section`
+  display: flex;
+  position: relative;
+  padding: 50px 100px;
+  box-sizing: border-box;
   width: 100%;
-  height: 1000px;
-`
+  justify-content: space-between;
+`;
 
-export default HeaderPresentational
+const Header_button = styled.input`
+  background-color: white;
+  outline: none;
+  border: none;
+  &:active {
+    transform: translateY(2px);
+  }
+`;
+const NAVIGATION = styled.div`
+  position: absolute;
+  top: 50px;
+  right: 100px;
+  width: 20%;
+  height: 100px;
+  background-color: white;
+`;
+const CLOSE = styled.button`
+  background-color: transparent;
+  float: right;
+  outline: none;
+  border: none;
+  &:active {
+    transform: translateY(2px);
+  }
+`;
+
+export default HeaderPresentational;
