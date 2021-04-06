8bfdc064c11d0834c4aa989e29b92c929252b385
zombietux <>
2021-04-06 15:36:27 +0900

    불필요한 코드 삭제

---
+++ ToDoListApp/ToDoListApp/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  ToDoListApp
+//
+//  Created by zombietux on 2021/04/06.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
