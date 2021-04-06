5b5c8f341cee7d218b4ad7b8f17c508fe9fa49d7
hiro <kiryuinh32@gmail.com>
2021-04-07 01:56:36 +0900

    init

--- BE/todo-list/src/main/java/team_16/todolist/TodoListApplication.java
+++ BE/todo-list/src/main/java/team_16/todolist/TodoListApplication.java
@@ -2,9 +2,7 @@ package team_16.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import team_16.todolist.config.ApplicationConfig;
+
 
 @SpringBootApplication
 public class TodoListApplication {
