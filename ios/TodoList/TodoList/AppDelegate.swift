d0a59e7ba9e417722ed320ed81299481b0980d18
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-04-06 16:26:05 +0900

    프로젝트 생성

---
+++ ios/TodoList/TodoList/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  TodoList
+//
+//  Created by Jun Ho JANG on 2021/04/06.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
