515f3a8d225453163eaf9e385e36db13440157b1
shimyoungmin <dudals6965@gmail.com>
2021-04-06 16:28:17 +0900

    TodoList 프로젝트 생성

---
+++ Todo_List/Todo_List/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Todo_List
+//
+//  Created by 심영민 on 2021/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ Todo_List/Todo_ListTests/Todo_ListTests.swift
@@ -0,0 +1,14 @@
+//
+//  Todo_ListTests.swift
+//  Todo_ListTests
+//
+//  Created by 심영민 on 2021/04/06.
+//
+
+import XCTest
+@testable import Todo_List
+
+class Todo_ListTests: XCTestCase {
+
+
+}
