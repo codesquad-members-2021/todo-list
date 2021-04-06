3c2ee21454e49ab7065af4551637cac670b94ceb
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-04-06 17:36:13 +0900

    JSON Parsing, DataManager 단위 테스트 작성

--- ios/TodoList/TodoListTests/TodoListTests.swift
+++ ios/TodoList/TodoListTests/TodoListTests.swift
@@ -8,25 +8,29 @@
 import XCTest
 
 class TodoListTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    override func setUp() {
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_JSONParser() {
+        let urlString = "https://jsonplaceholder.typicode.com/todos/1"
+        if let url = URL(string: urlString) {
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let data = data {
+                    XCTAssertNotNil(JSONParser.parseData(data: data))
+                }
+            }.resume()
         }
+    }
+    
+    func test_DataManager() {
+        let urlString = "https://jsonplaceholder.typicode.com/todos/1"
+        XCTAssertNotNil(DataManager.getData(urlString: urlString, completion: { (data) in
+            
+        }))
     }
 
 }
