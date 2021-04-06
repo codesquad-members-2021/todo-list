a47bd8ae9d16924f250f6f7d520ccf2c8688ff55
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-04-06 17:34:25 +0900

    JSONParser

--- ios/TodoList/TodoList/Model/JSONParser.swift
+++ ios/TodoList/TodoList/Model/JSONParser.swift
@@ -9,6 +9,15 @@ import Foundation
 
 class JSONParser {
     
-    
+    static func parseData(data: Data) -> ToDoItem? {
+        let decoder = JSONDecoder()
+        do {
+            let result = try decoder.decode(ToDoItem.self, from: data)
+            return result
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
     
 }
