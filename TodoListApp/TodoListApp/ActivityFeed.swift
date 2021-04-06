d141db878d17bfccc47e9f440f2019b2194048b1
1song2 <1song2918@gmail.com>
2021-04-06 20:19:59 +0900

    Add class 'ActivityFeed'

---
+++ TodoListApp/TodoListApp/ActivityFeed.swift
@@ -0,0 +1,20 @@
+//
+//  ActivityFeed.swift
+//  TodoListApp
+//
+//  Created by Song on 2021/04/06.
+//
+
+import Foundation
+
+class ActivityFeed {
+    var userActions: [Activity]
+    
+    init(userActions: [Activity]) {
+        self.userActions = userActions
+    }
+    
+    func add(_ activity: Activity) {
+        userActions.append(activity)
+    }
+}
