d98cd799aa8b94627a0e0577e0b83425195dc9e8
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-04-06 22:06:45 +0900

    fix : 누락된 어노테이션 추가

--- be/todo/src/main/java/com/codesquad/todo/web/controller/ColumnController.java
+++ be/todo/src/main/java/com/codesquad/todo/web/controller/ColumnController.java
@@ -1,13 +1,13 @@
 package com.codesquad.todo.web.controller;
 
 import com.codesquad.todo.web.domain.Column;
-import com.codesquad.todo.web.domain.Task;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 @RequestMapping("/columns")
 public class ColumnController {
 
