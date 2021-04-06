52bb2cb9fdbe91c87db7a0cb4353022ab63581db
gisungchoi <chlrltjd159@naver.com>
2021-04-06 17:57:03 +0900

    feat : spring boot mysql 기본 설정
    -데이터 소스 설정

---
+++ backend/src/test/java/com/codeSquad/cocokyu/CocoKyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeSquad.cocokyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocoKyuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
