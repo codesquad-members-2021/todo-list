979c5f83c3640690a8c573395aa6d235d85f1c73
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-04-07 04:46:04 +0900

    [Add] UserList.js 1-11 배열 랜더링하기

--- todo-eamon/src/App.js
+++ todo-eamon/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Hello from "./Hello";
-import Counter from "./Counter";
-import InputSample from "./inputSample";
+// import Hello from "./Hello";
+// import Counter from "./Counter";
+// import InputSample from "./inputSample";
+import UserList from "./UserList";
 
 function App() {
-  return <InputSample />;
+  return <UserList />;
 }
 
 export default App;
