ae487f4e76e0cdd0fee708bb83aed8f21d891207
Sally Oh <min27604@hotmail.com>
2021-04-07 18:17:13 +0900

    feat: 아이디로 태스크 조회기능 구현
    URL로 들어온 아이디를 이용해 새로운 태스크 객체를 만들어 반환

--- backend/todo-list-backend/src/main/java/com/codesquad/esfj/todolist/task/Task.java
+++ backend/todo-list-backend/src/main/java/com/codesquad/esfj/todolist/task/Task.java
@@ -2,14 +2,20 @@ package com.codesquad.esfj.todolist.task;
 
 public class Task {
 
+    private Long id;
     private String title;
     private String content;
     private String writer;
 
-    public Task(String title, String content, String writer) {
+    public Task(Long id, String title, String content, String writer) {
+        this.id = id;
         this.title = title;
         this.content = content;
         this.writer = writer;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getTitle() {
--- backend/todo-list-backend/src/main/java/com/codesquad/esfj/todolist/task/TaskController.java
+++ backend/todo-list-backend/src/main/java/com/codesquad/esfj/todolist/task/TaskController.java
@@ -1,6 +1,7 @@
 package com.codesquad.esfj.todolist.task;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
@@ -10,8 +11,13 @@ public class TaskController {
 
     @GetMapping("/tasks")
     public List<Task> readAll() {
-        return Arrays.asList(new Task("title1", "content1", "writer1"),
-                new Task("title2", "content2", "writer2"),
-                new Task("title3", "content3", "writer3"));
+        return Arrays.asList(new Task(1L, "title1", "content1", "writer1"),
+                new Task(2L, "title2", "content2", "writer2"),
+                new Task(3L, "title3", "content3", "writer3"));
+    }
+
+    @GetMapping("/tasks/{id}")
+    public Task readOne(@PathVariable Long id) {
+        return new Task(id, "title", "content", "writer");
     }
 }
