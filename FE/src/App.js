a02b37e5592fe2ede76aed6538b300d1262cf22d
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-04-07 18:00:59 +0900

    Add : 투두리스트 헤더 추가

---
+++ FE/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Header/Header.js';
+import TodoList from './TodoList/TodoList.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ FE/src/atoms/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Menu.css'
+
+function Menu() {
+  return (
+    <div className="Menu">
+       <img src ="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png"></img>
+    </div>
+  );
+}
+
+export default Menu;
