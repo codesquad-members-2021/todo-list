a829cf4ef43d3d33eb88db6ae435eb4379b1bf88
BMO <b38238496@gmail.com>
2021-04-08 02:15:47 +0900

    [#5] feat: TableView Card Cell 구현
    Label 3개 작성(title, contents, author)
    contents label self-resize
    contents label 3줄 제한
    cell 간격 임시 구현(수정 필요)

--- ToDoListApp/ToDoListApp/ToDoController.swift
+++ ToDoListApp/ToDoListApp/ToDoController.swift
@@ -9,21 +9,44 @@ import UIKit
 
 class ToDoController: UIViewController {
 
+    @IBOutlet weak var todoTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        configureCell()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureCell() {
+        let nibName = UINib(nibName: "CardCell", bundle: nil)
+        todoTableView.register(nibName, forCellReuseIdentifier: "cardCell")
+        todoTableView.rowHeight = UITableView.automaticDimension
+        todoTableView.estimatedRowHeight = 108
+        todoTableView.separatorStyle = .none
+        
     }
-    */
 
+}
+
+extension ToDoController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        view.tintColor = UIColor.systemGray6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = todoTableView.dequeueReusableCell(withIdentifier: "cardCell") as! CardCell
+        return cell
+    }
 }
