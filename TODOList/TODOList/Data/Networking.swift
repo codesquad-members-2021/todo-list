6a8d596ed3c20cdee4c99775c19fded9570dfc86
okstring <milkoi7801@gmail.com>
2021-04-07 17:52:18 +0900

    Merge branch

--- TODOList/TODOList/Data/Networking.swift
+++ TODOList/TODOList/Data/Networking.swift
@@ -15,26 +15,26 @@ class Networking {
         session = .shared
         self.baseURL = baseURL
     }
-    
-    func getToDoList(completionHandler: @escaping (Data)->Void) {
-        guard let url = URL(string: baseURL) else { return }
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        SessionManger.request(urlRequest: request) { (data) in
-            let decodedData = DataManager.decoding(decodable: <#T##Decodable.Protocol#>, data: data)
-            completionHandler(decodedData)
-        }
-    }
-    
-    func postToDoList(comletionHandler: @escaping (Data)->Void) {
-        guard let url = URL(string: baseURL) else { return }
-        var request = URLRequest(url: url)
-        let encodedData = DataManager.encoding(encodable: <#T##Encodable#>)
-        request.httpMethod = "POST"
-        request.httpBody = encodedData
-        SessionManger.request(urlRequest: request) { (data) in
-            let decodedData = DataManager.decoding(decodable: <#T##Decodable.Protocol#>, data: data)
-            completionHandler(decodedData)
-        }
-    }
+//    
+//    func getToDoList(completionHandler: @escaping (Data)->Void) {
+//        guard let url = URL(string: baseURL) else { return }
+//        var request = URLRequest(url: url)
+//        request.httpMethod = "GET"
+//        SessionManger.request(urlRequest: request) { (data) in
+//            let decodedData = DataManager.decoding(decodable: <#T##Decodable.Protocol#>, data: data)
+//            completionHandler(decodedData)
+//        }
+//    }
+//    
+//    func postToDoList(comletionHandler: @escaping (Data)->Void) {
+//        guard let url = URL(string: baseURL) else { return }
+//        var request = URLRequest(url: url)
+//        let encodedData = DataManager.encoding(encodable: <#T##Encodable#>)
+//        request.httpMethod = "POST"
+//        request.httpBody = encodedData
+//        SessionManger.request(urlRequest: request) { (data) in
+//            let decodedData = DataManager.decoding(decodable: <#T##Decodable.Protocol#>, data: data)
+//            completionHandler(decodedData)
+//        }
+//    }
 }
