81bf61eb1a996a2bb789cf7f1ab3a1a581906128
bibi <bibi6666667@kakao.com>
2021-04-07 15:39:10 +0900

    Feat : 테스트 데이터 조회를 위한 CardController 메서드 수정

--- src/main/java/codesquad/TodoList/controller/CardController.java
+++ src/main/java/codesquad/TodoList/controller/CardController.java
@@ -1,22 +1,34 @@
 package codesquad.TodoList.controller;
 
+import codesquad.TodoList.domain.Card;
+import codesquad.TodoList.repository.CardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/cards")
 public class CardController {
 
+    private CardRepository cardRepository;
+
+    @Autowired
+    public CardController(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
+
     // CREATE
     @PostMapping
-    public void newCard() {
-
+    public void newCard(@RequestParam("card") Card card) {
+        cardRepository.save(card);
     }
     // READ
     @GetMapping
     @ResponseBody
-    public void showCards() {
-
+    public List<Card> showCards() {
+         return cardRepository.findAllByTodoTrueOrDoingTrueOrDoneTrue();
     }
     // MOVE
     @PutMapping("/{id}/status")
