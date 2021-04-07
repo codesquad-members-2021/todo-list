5a302ca9348736ae19f1df564789ca121feae888
okstring <milkoi7801@gmail.com>
2021-04-07 12:01:30 +0900

    Test, Add: Use prepare(not passing data)

--- TODOList/TODOList/SectionViewController.swift
+++ TODOList/TODOList/SectionViewController.swift
@@ -7,9 +7,44 @@
 
 import UIKit
 
-class SectionViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+protocol CellInfoDelegate {
+    func sendData()
+}
+
+class State {
+    var subject: [String]
+    var body: [String]
+    
+    init() {
+        self.subject = [String]()
+        self.body = [String]()
     }
 }
+
+enum Mode {
+    case willTODO
+    case doingTODO
+    case completeTODO
+}
+
+class SectionViewController: UIViewController {
+    @IBOutlet weak var TODOTableView: UITableView!
+    @IBOutlet var TODOTableViewDataSource: TODOTableViewDataSource!
+    @IBOutlet weak var sectionTitle: UILabel!
+    @IBOutlet weak var TODOCount: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    var mode: Mode?
+    
+    var handler: ((State) -> ())? {
+        didSet {
+            self.TODOTableView.reloadData()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
+
