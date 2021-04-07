7840730ef3c80febc7a9933f483be9ba01eefc0e
Lia <judykim257@gmail.com>
2021-04-07 18:26:21 +0900

    VC - model로 부터 값을 가져오게 구현

--- Todo_List/Todo_List/TodoDataSource.swift
+++ Todo_List/Todo_List/TodoDataSource.swift
@@ -8,26 +8,31 @@
 import UIKit
 
 class TodoDataSource: NSObject, UITableViewDataSource {
-    private var todoCards: [TodoCard] = []
+    private var todoCards: TodoCardsManageable
+    
+    init(todoCards: TodoCardsManageable) {
+        self.todoCards = todoCards
+        super.init()
+    }
+    
+    convenience override init() {
+        self.init(todoCards: TodoCards())
+    }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5//todoCards.count
+        return todoCards.countCards()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: TodoCell.identifier, for: indexPath) as? TodoCell else { return UITableViewCell() }
-        cell.titleLabel.text = titleEX
-        cell.contentLabel.text = contentEX
+        
+        cell.titleLabel.text = todoCards.getCard(at: indexPath).title
+        cell.contentLabel.text = todoCards.getCard(at: indexPath).content
         cell.contentLabel.addLineSpacing()
         
         return cell
     }
 }
 
-let titleEX = "GitHub 공부하기"
-let contentEX = """
-            add, commit, push
-            * GitHub 공부 내용
-            * 모던 스위프트 공부 내용 1장
-            - JK 의 앵프라맹스
-            """
+
