a4eb9a52c3dc0f1df27831f48a64ab3001559129
Lia <judykim257@gmail.com>
2021-04-07 16:53:20 +0900

    to-do TableView ViewController에 등록 및 autoSize 구현

--- Todo_List/Todo_List/TodoDelegate.swift
+++ Todo_List/Todo_List/TodoDelegate.swift
@@ -1,5 +1,5 @@
 //
-//  TodoTableViewDelegate.swift
+//  TodoDelegate.swift
 //  Todo_List
 //
 //  Created by 심영민 on 2021/04/06.
@@ -10,6 +10,6 @@ import UIKit
 class TodoDelegate: NSObject, UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        UITableView.automaticDimension
+        return UITableView.automaticDimension
     }
 }
--- Todo_List/Todo_List/TodoTableViewController.swift
+++ Todo_List/Todo_List/TodoTableViewController.swift
@@ -9,8 +9,22 @@ import UIKit
 
 class TodoTableViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    private lazy var tableViewDelegate = TodoDelegate()
+    private var todoDataSource = TodoDataSource()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setting()
+    }
     
+    private func setting() {
+        tableView.dataSource = todoDataSource
+        tableView.delegate = tableViewDelegate
+        tableView.register(UINib(nibName: TodoCell.identifier, bundle: nil), forCellReuseIdentifier: TodoCell.identifier)
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = UITableView.automaticDimension
     }
 }
