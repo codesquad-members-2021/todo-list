7d1024b1eace767e7272b39cae95153ff2a55bb1
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-04-07 09:13:27 +0900

    [Add]TodoTemplate.js
    - [Style] .TodoTemplate div css

--- todo-eamon/src/App.js
+++ todo-eamon/src/App.js
@@ -2,66 +2,16 @@ import React, { useRef, useState } from 'react';
 // import Hello from "./Hello";
 // import Counter from "./Counter";
 // import InputSample from "./inputSample";
-import UserList from './UserList';
-import CreateUser from './CreateUser';
-
+// import UserList from './UserList';
+// import CreateUser from './CreateUser';
+import TodoTemplate from './components/TodoTemplate';
 function App() {
-  const [inputs, setInputs] = useState({
-    username: '',
-    email: ''
-  });
-  const { username, email } = inputs;
-  const onChange = e => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    });
-  };
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com'
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com'
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com'
-    }
-  ]);
-
-  const nextId = useRef(4);
-  const onCreate = () => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    };
-    setUsers(users.concat(user));
-
-    setInputs({
-      username: '',
-      email: ''
-    });
-    nextId.current += 1;
-  };
-  return (
-    <>
-      <CreateUser
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList users={users} />
-    </>
-  );
-}
-
-export default App;
+    return (
+      <>
+        <TodoTemplate>안녕하세요</TodoTemplate>
+      </>
+    );
+  }
+  
+  export default App;
+  
---
+++ todo-eamon/src/components/TodoTemplate.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TodoTemplate({ children }) {
+  return <div className={"TodoTemplate"}>{children}</div>;
+}
+
+export default TodoTemplate;
