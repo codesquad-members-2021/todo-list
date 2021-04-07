ec62d0b52b54e1865ee98ccaeb05d7399c58141a
jhpark-steve <park9368@naver.com>
2021-04-07 18:23:06 +0900

    refactor: transform UIcode of columns to storyboard (#2)
    problem: containerView의 ColumnViewController에서 tableView의 delegate datasource 연결하려고 하였으나 에러로 실패

--- frontend/ToDoListApp/ToDoListApp/ColumnSection/ColumnTitle.swift
+++ frontend/ToDoListApp/ToDoListApp/ColumnSection/ColumnTitle.swift
@@ -5,4 +5,18 @@
 //  Created by 박정하 on 2021/04/07.
 //
 
-import Foundation
+import UIKit
+
+class ColumnTitle : UILabel{
+    init(frame: CGRect, title: String) {
+        super.init(frame: frame)
+        self.text = title
+        self.sizeToFit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.text = ""
+        self.sizeToFit()
+    }
+}
--- frontend/ToDoListApp/ToDoListApp/ColumnSection/ColumnViewController.swift
+++ frontend/ToDoListApp/ToDoListApp/ColumnSection/ColumnViewController.swift
@@ -8,8 +8,28 @@
 import UIKit
 
 class ColumnViewController : UIViewController{
+    @IBOutlet weak var columnTitle: UILabel!
+    @IBOutlet weak var columnTableView: UITableView!
+    private let columnDataSource : ColumnDataSource//원하시면 변수명 변경하셔도 됩니당
+    private let columnDelegate : ColumnDelegate //일단은 뷰컨 - 뷰 일대일 대응 방식으로 코드 구성해놓을게요
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        columnDataSource = ColumnDataSource.init(columnidentifier: CellIdentifiers.column1.rawValue, datamanager: ColumnDatas())
+        columnDelegate = ColumnDelegate.init()
+        columnTableView.delegate = columnDelegate
+        columnTableView.dataSource = columnDataSource
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        columnDataSource = ColumnDataSource.init(columnidentifier: CellIdentifiers.column1.rawValue, datamanager: ColumnDatas())
+        columnDelegate = ColumnDelegate.init()
+        columnTableView.delegate = columnDelegate
+        columnTableView.dataSource = columnDataSource
+        super.init(coder: coder)
+    }
+    
     override func viewDidLoad() {
-        self.view.addSubview(Column.init(frame: CGRect(x: 48, y: 123, width: 256, height: 294), Cellidentifier: CellIdentifiers.column1.rawValue, HeaderTitle: "해야할 일"))
         super.viewDidLoad()
     }
 }
