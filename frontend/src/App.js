2c3bc895fcafb0b7d703c5af0a241ca817bc3f71
Hoyoung Son <xzxking17@gmail.com>
2021-04-07 21:21:35 +0900

    feat: Add Header - 전체적인 틀 & 헤더 완성     - 메뉴버튼 누를 시 SideLog visible     - SideLog에서 닫기버튼 누를 시 SideLog invisible

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,15 @@
-import React from 'react';
-import TestContainer from "./containers/Test/TestContainer";
+import React, { useEffect } from 'react';
+import { MainWrapper } from './components/common/StyledMain';
+import HeaderContainer from './containers/header/HeaderContainer';
 
-const App = () => <TestContainer />;
+const App = () => {
+    useEffect(() => (document.body.style = 'background: #F5F5F7;'), []);
+
+    return (
+        <MainWrapper>
+            <HeaderContainer />
+        </MainWrapper>
+    );
+};
 
 export default App;
