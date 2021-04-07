37908f74f2424340cda03946227ddb80aded96b1
Lia <judykim257@gmail.com>
2021-04-07 16:51:29 +0900

    to-do card model

--- Todo_List/Todo_List/TodoCard.swift
+++ Todo_List/Todo_List/TodoCard.swift
@@ -2,7 +2,28 @@
 //  TodoCard.swift
 //  Todo_List
 //
-//  Created by 김지경 on 2021/04/06.
+//  Created by Lia on 2021/04/06.
 //
 
 import Foundation
+
+class TodoCard {
+    
+    private var title: String
+    private var content: String
+    
+    init(title: String, content: String) {
+        self.title = title
+        self.content = content
+    }
+    
+    convenience init() {
+        let title = ""
+        let content = ""
+        self.init(title: title, content: content)
+    }
+    
+    
+}
+
+
