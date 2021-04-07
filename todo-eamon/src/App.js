33860150f629619e08345935a67bb5b5ee502018
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-04-07 09:23:01 +0900

    [Add]TodoHead.js
    - [Style] TodoHead css

--- todo-eamon/src/App.js
+++ todo-eamon/src/App.js
@@ -1,17 +1,19 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 // import Hello from "./Hello";
 // import Counter from "./Counter";
 // import InputSample from "./inputSample";
 // import UserList from './UserList';
 // import CreateUser from './CreateUser';
-import TodoTemplate from './components/TodoTemplate';
+import TodoTemplate from "./components/TodoTemplate";
+import TodoHead from "./components/TodoHead";
 function App() {
-    return (
-      <>
-        <TodoTemplate>안녕하세요</TodoTemplate>
-      </>
-    );
-  }
-  
-  export default App;
-  
+  return (
+    <>
+      <TodoTemplate>
+        <TodoHead />
+      </TodoTemplate>
+    </>
+  );
+}
+
+export default App;
---
+++ todo-eamon/src/components/TodoHead.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+
+function TodoHead() {
+  return (
+    <div className="TodoHead">
+      <h1 className="h1">2019년 7월 10일</h1>
+      <div className="day">수요일</div>
+      <div className="tasks-left">할 일 2개 남음</div>
+    </div>
+  );
+}
+
+export default TodoHead;
