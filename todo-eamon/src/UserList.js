523e5f666332e019c3811f5d542da16db8d646e9
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-04-07 09:03:25 +0900

    [Add]CreateUser.js 1-13 배열 항목 추가하기

--- todo-eamon/src/UserList.js
+++ todo-eamon/src/UserList.js
@@ -8,24 +8,8 @@ function User({ user }) {
   );
 }
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: "velopert",
-      email: "public.velopert@gmail.com",
-    },
-    {
-      id: 2,
-      username: "tester",
-      email: "tester@example.com",
-    },
-    {
-      id: 3,
-      username: "liz",
-      email: "liz@example.com",
-    },
-  ];
+function UserList({ users }) {
+  
 
   return (
     <div>
