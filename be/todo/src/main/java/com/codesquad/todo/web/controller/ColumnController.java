c26806188654d575b2675f177924488fddb895b2
jung <rla36925@gmail.com>
2021-04-08 16:08:55 +0900

    feat : ColumDto와 TaskDto 추가해 응답포맷 최적화

--- be/todo/src/main/java/com/codesquad/todo/web/controller/ColumnController.java
+++ be/todo/src/main/java/com/codesquad/todo/web/controller/ColumnController.java
@@ -3,6 +3,7 @@ package com.codesquad.todo.web.controller;
 import com.codesquad.todo.web.domain.Column;
 import com.codesquad.todo.web.domain.Task;
 import com.codesquad.todo.web.domain.User;
+import com.codesquad.todo.web.service.dto.ColumnDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,19 +16,20 @@ import java.util.List;
 public class ColumnController {
 
     @GetMapping
-    public List<Column> showColumnList() {
-        List<Column> columnList = new ArrayList<>();
+    public List<ColumnDto> showColumnList() {
+        List<ColumnDto> columnList = new ArrayList<>();
         User author = new User(1, "라쿠운", "Racoon", "1234", "");
 
         Column todoColumn = new Column(1, "TODO");
         todoColumn.getTaskList().add(new Task(1, "task1", "taskContent1", author, todoColumn));
         todoColumn.getTaskList().add(new Task(2, "task2", "taskContent2", author, todoColumn));
-        columnList.add(todoColumn);
+        columnList.add(new ColumnDto(todoColumn));
 
         Column inProgressColumn = new Column(2, "IN_PROGRESS");
         inProgressColumn.getTaskList().add(new Task(3, "task3", "taskContent3", author, inProgressColumn));
-        columnList.add(inProgressColumn);
+        columnList.add(new ColumnDto(inProgressColumn));
 
         return columnList;
     }
+
 }
