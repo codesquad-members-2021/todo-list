459570977dbd8eff4a134d50b9dce85132911f7b
Hannah Park <ha3158987@gmail.com>
2021-04-08 18:52:29 +0900

    [#7] feat: :hamster: 아이콘 컴포넌트 생성, styled components 설치

--- FE/todolist/src/utilComponent/Icon.js
+++ FE/todolist/src/utilComponent/Icon.js
@@ -1,13 +1,50 @@
-const Icon = function () {
+import styled from 'styled-components'
+
+// grey : #BDBDBD
+// blue : #0075DE
+// red : #FF4343
+const IconButton = styled.svg`
+    path {
+        fill: ${props => props.type === 'bars' ? 'black' : '#BDBDBD'};
+    }
+`
+
+const PlusIcon = ({type}) => {
     return (
-        <>
-        <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <IconButton type={type}>
+            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M0.105709 7.53033L0.105709 6.46967H6.46967V0.105713H7.53033V6.46967H13.8943V7.53033H7.53033V13.8943H6.46967V7.53033H0.105709Z" fill="black"/>
-        </svg>
-        </>
-    );
+            </svg>
+        </IconButton>
+    )
 }
 
+const CloseIcon = ({type}) => {
+    return (
+        <IconButton type={type}>
+            <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1.5 11.25L0.75 10.5L5.25 6L0.75 1.5L1.5 0.75L6 5.25L10.5 0.75L11.25 1.5L6.75 6L11.25 10.5L10.5 11.25L6 6.75L1.5 11.25Z" fill="black"/>
+            </svg>
+        </IconButton>
+    )
+}
 
+const BarsIcon = ({type}) => {
+    return (
+        <IconButton type={type}>
+            <svg width="17" height="11" viewBox="0 0 17 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 1V0H17V1H0ZM17 5V6H0V5H17ZM0 10H17V11H0V10Z" fill="black"/>
+            </svg>
+        </IconButton>
+    )
+}
+
+const Icon = ({ type }) => {
+    return {
+        plus: <PlusIcon type={ type }/>,
+        close: <CloseIcon type={ type }/>,
+        bars: <BarsIcon type={ type }/>
+    }[type];
+}
 
 export default Icon;
