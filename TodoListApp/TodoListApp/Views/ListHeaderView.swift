112b717066f77df1c4f663002225e87b47626812
1song2 <1song2918@gmail.com>
2021-04-08 11:11:39 +0900

    Add custom UIView 'ListHeaderView'

---
+++ TodoListApp/TodoListApp/Views/ListHeaderView.swift
@@ -0,0 +1,45 @@
+//
+//  ListHeaderView.swift
+//  TodoListApp
+//
+//  Created by Song on 2021/04/07.
+//
+
+import UIKit
+
+class ListHeaderView: UIView {
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var badgeButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    
+    var title: String? {
+        get {
+            return titleLabel.text
+        }
+        set {
+            titleLabel.text = newValue
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        let nib = UINib(nibName: "ListHeaderView", bundle: nil)
+        nib.instantiate(withOwner: self, options: nil)
+        contentView.frame = bounds
+        addSubview(contentView)
+        
+        badgeButton.widthAnchor.constraint(greaterThanOrEqualTo: badgeButton.heightAnchor).isActive = true
+        badgeButton.isEnabled = false
+        badgeButton.setTitleColor(.black, for: .disabled)
+    }
+}
