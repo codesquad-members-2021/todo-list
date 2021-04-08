1d9c83d30ecec2a28969a945affefc6ccb0a05c5
jeong-inho <derosatam76@gmail.com>
2021-04-08 11:19:46 +0900

    Docs : 붎필요한 파일 삭제

--- BE/src/main/resources/data.sql
+++ BE/src/main/resources/data.sql
@@ -1,1 +1,1 @@
-insert into user(name, email, created_date) values ('Zero Jung', 'honux@gmail.com', getdate());
+insert into user(name, email, created_date) values ('Zero Jung', 'honux@gmail.com', '2021-04-07 10:10:10');
