2828791065dc02dd846182a7a90f8297f39fd545
Hongbi <dyongdi@gmail.com>
2021-04-08 17:25:49 +0900

    add: icon 컴포넌트 hover 스타일 적용

--- fe/todo-list/src/components/icon/icon.jsx
+++ fe/todo-list/src/components/icon/icon.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const AddButton = () => {
+const AddButton = ({ type }) => {
   return (
-    <IconButton>
+    <IconButton type={type}>
       <i className="fas fa-plus"></i>
     </IconButton>
   );
 };
-const DeleteButton = () => {
+const DeleteButton = ({ type }) => {
   return (
-    <IconButton>
+    <IconButton type={type}>
       <i className="fas fa-times"></i>
     </IconButton>
   );
 };
-const UserActionButton = () => {
+const UserActionButton = ({ type }) => {
   return (
-    <IconButton>
+    <IconButton type={type}>
       <i className="fas fa-bars"></i>
     </IconButton>
   );
@@ -25,16 +25,25 @@ const UserActionButton = () => {
 
 const Icon = ({ type }) => {
   return {
-    add: <AddButton />,
-    delete: <DeleteButton />,
-    userAction: <UserActionButton />,
+    add: <AddButton type={type} />,
+    delete: <DeleteButton type={type} />,
+    userAction: <UserActionButton type={type} />,
   }[type];
 };
 
 export default Icon;
 
 const IconButton = styled.button`
-  color: #bdbdbd;
+  color: ${props => (props.type === 'userAction' ? '#010101' : '#bdbdbd')};
   border: none;
   background-color: transparent;
+  outline: none;
+  cursor: pointer;
+  font-size: 16px;
+
+  &:hover {
+    color: ${props => {
+      return { add: '#0075DE', delete: '#FF4343' }[props.type] || '#505050';
+    }};
+  }
 `;
