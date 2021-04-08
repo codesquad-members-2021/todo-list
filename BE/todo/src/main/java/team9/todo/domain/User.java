8f894dc1b7e7708a38f8c695e1dbece809181525
정이삭 <isaac56@naver.com>
2021-04-08 21:37:17 +0900

    feat: Add getter into User
    User 멤버변수의 getter 메소드 추가

--- BE/todo/src/main/java/team9/todo/domain/User.java
+++ BE/todo/src/main/java/team9/todo/domain/User.java
@@ -12,12 +12,36 @@ public class User {
     private String name;
     private LocalDate createdDate;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
     public void setEmail(String email) {
         this.email = email;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
     }
 
     @Override
