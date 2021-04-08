06a08481163a498fc1f5ddfcf4c8afb87177776c
Jisoo Kim <ghis22130@naver.com>
2021-04-08 17:18:33 +0900

    chore: Add TableView Delgate, DataSource
    InProgress, Done TableView의 Delegate, DataSource 추가

---
+++ ToDoListApp/ToDoListApp/Controller/InProgressTableViewDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  InProgressTableViewDelegate.swift
+//  ToDoListApp
+//
+//  Created by 지북 on 2021/04/08.
+//
+
+import Foundation
+import UIKit
+
+class InProgressTableViewDelegate: NSObject, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        view.tintColor = UIColor.systemGray6
+    }
+}
