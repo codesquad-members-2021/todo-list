ed136313d590787e8b65c491e3220a8c51f6aa64
shimyoungmin <dudals6965@gmail.com>
2021-04-08 16:19:31 +0900

    추가 ModalView 생성

---
+++ Todo_List/Todo_List/ModalViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ModalViewController.swift
+//  Todo_List
+//
+//  Created by 심영민 on 2021/04/08.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Todo_List/Todo_List/TodoTableViewController.swift
+++ Todo_List/Todo_List/TodoTableViewController.swift
@@ -12,6 +12,7 @@ class TodoTableViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var columnNameLabel: UILabel!
     @IBOutlet weak var cardNumLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
     
     private lazy var tableViewDelegate = TodoDelegate()
     private var todoDataSource = TodoDataSource()
@@ -28,5 +29,16 @@ class TodoTableViewController: UIViewController {
         tableView.register(UINib(nibName: TodoCell.identifier, bundle: nil), forCellReuseIdentifier: TodoCell.identifier)
         
         cardNumLabel.text = "\(tableView.numberOfRows(inSection: 0))"
+    }
+    
+    @IBAction func addButtonTouched(_ sender: UIButton) {
+        let modalview = ModalViewController(nibName: "ModalViewController", bundle: nil)
+        showPopup(modalview)
+        self.present(modalview, animated: true, completion: nil)
+    }
+    
+    private func showPopup(_ controller: UIViewController) {
+        controller.modalPresentationStyle = .custom
+        controller.preferredContentSize = CGSize(width: 200, height: 200)
     }
 }
