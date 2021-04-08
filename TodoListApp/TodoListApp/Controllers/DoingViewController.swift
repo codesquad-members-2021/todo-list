217a8a69a467b594166665222e64a6bf7a1875fc
1song2 <1song2918@gmail.com>
2021-04-08 12:23:40 +0900

    Make lists show cards

--- TodoListApp/TodoListApp/Controllers/DoingViewController.swift
+++ TodoListApp/TodoListApp/Controllers/DoingViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class DoingViewController: UIViewController {
     @IBOutlet weak var headerView: ListHeaderView!
+    @IBOutlet weak var cardTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,5 +17,21 @@ class DoingViewController: UIViewController {
         headerView.badgeButton.setTitle("1", for: .normal)
         headerView.badgeButton.layer.masksToBounds = true
         headerView.badgeButton.layer.cornerRadius = headerView.frame.size.height / 2
+        
+        cardTableView.dataSource = self
+        cardTableView.register(CardCell.nib(), forCellReuseIdentifier: CardCell.identifier)
+    }
+}
+
+extension DoingViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell  = tableView.dequeueReusableCell(withIdentifier: CardCell.identifier, for: indexPath) as! CardCell
+        cell.titleLabel.text = "HTML/CSS 공부하기"
+        cell.descriptionLabel.text = "input 태그 실습"
+        return cell
     }
 }
--- TodoListApp/TodoListApp/Controllers/DoneViewController.swift
+++ TodoListApp/TodoListApp/Controllers/DoneViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class DoneViewController: UIViewController {
     @IBOutlet weak var headerView: ListHeaderView!
+    @IBOutlet weak var cardTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,5 +17,21 @@ class DoneViewController: UIViewController {
         headerView.badgeButton.setTitle("0", for: .normal)
         headerView.badgeButton.layer.masksToBounds = true
         headerView.badgeButton.layer.cornerRadius = headerView.frame.size.height / 2
+        
+        cardTableView.dataSource = self
+        cardTableView.register(CardCell.nib(), forCellReuseIdentifier: CardCell.identifier)
+    }
+}
+
+extension DoneViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell  = tableView.dequeueReusableCell(withIdentifier: CardCell.identifier, for: indexPath) as! CardCell
+        cell.titleLabel.text = "GitHub 공부하기"
+        cell.descriptionLabel.text = "add, commit, push"
+        return cell
     }
 }
