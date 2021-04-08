7cb062aa3700968da4a8ac5ce4e85138bff879dd
Jungwon Lee <leejungwon96@gmail.com>
2021-04-08 17:15:36 +0900

    [feat] todolist 추가, 삭제 구현

--- frontend/todo-frontend/src/component/todoMain/TodoMain.jsx
+++ frontend/todo-frontend/src/component/todoMain/TodoMain.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import TodoList from "./todoList/TodoList";
 
-const TodoMain = (props) => {
-  return <TodoList />;
+const TodoMain = ({ datas }) => {
+  const todoColumneList = datas.todoData.map((data) => (
+    <TodoList data={data} />
+  ));
+
+  return <div>{todoColumneList}</div>;
 };
 export default TodoMain;
--- frontend/todo-frontend/src/component/todoMain/todoList/TodoList.jsx
+++ frontend/todo-frontend/src/component/todoMain/todoList/TodoList.jsx
@@ -1,1 +1,31 @@
+import React, { useState, useCallback } from "react";
+import TodoItem from "./TodoItem";
+import TodoListForm from "./TodoListForm";
 
+const TodoList = ({ data: { id, title, todoCards } }) => {
+  const [todos, setTodos] = useState(todoCards);
+
+  const addTodoItem = (todoCard) => {
+    setTodos((todos) => [...todos, todoCard]);
+  };
+
+  const deleteTodoItem = (id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  };
+
+  const todoCardList = todos.map((card) => (
+    <TodoItem todoCard={card} deleteTodoItem={deleteTodoItem} />
+  ));
+  return (
+    <div>
+      <div>
+        {title} {todos.length}개
+      </div>
+
+      <TodoListForm addTodoItem={addTodoItem} />
+      <div>{todoCardList}</div>
+    </div>
+  );
+};
+
+export default TodoList;
