a0ea1298d30418962ddc2e8e533fe73e7ab540a0
dahun-lee-daji <ssss6596@gmail.com>
2021-04-08 10:56:30 +0900

    Refactor : Change how to loadContainerView (#2)
    스토리보드에서 ContainerView를 embed
    -> ViewController에서 ContainerView를 Code로 embed
    변수 명 수정 필요

--- frontend/ToDoListApp/ToDoListApp/ColumnSection/Controllers/ColumnViewController.swift
+++ frontend/ToDoListApp/ToDoListApp/ColumnSection/Controllers/ColumnViewController.swift
@@ -10,12 +10,32 @@ import UIKit
 class ColumnViewController : UIViewController {
     @IBOutlet weak var columnTitle: UILabel!
     @IBOutlet weak var columnTableView: UITableView!
+    
     private let columnDataSource = ColumnDataSource(datamanager: ColumnDatas())//원하시면 변수명 변경하셔도 됩니당
     private let columnDelegate = ColumnDelegate() //일단은 뷰컨 - 뷰 일대일 대응 방식으로 코드 구성해놓을게요
     
+    var temptitle = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        columnTitle.text = temptitle
+        guard columnTableView != nil else {return}
+        
         columnTableView.delegate = columnDelegate
         columnTableView.dataSource = columnDataSource
+        columnTableView.reloadData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+    }
+    
+    override func viewLayoutMarginsDidChange() {
+        
     }
 }
--- frontend/ToDoListApp/ToDoListApp/ViewController.swift
+++ frontend/ToDoListApp/ToDoListApp/ViewController.swift
@@ -7,16 +7,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {    
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        super.init(nibName: nil, bundle: nil)
-    }
+class ViewController: UIViewController {
     
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-    }
-    
+    @IBOutlet weak var toDoColumn: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
+        
+        let columnViewStoryboard = UIStoryboard.init(name: "ColumnView", bundle: nil)
+        let columnVC = columnViewStoryboard.instantiateViewController(identifier: "ColumnView") as ColumnViewController
+        columnVC.temptitle = "test"
+        self.addChild(columnVC)
+        toDoColumn.addSubview(columnVC.view!)
+        }
+    
+//    override func viewDidAppear(_ animated: Bool) {
+//
+//    }
 }
