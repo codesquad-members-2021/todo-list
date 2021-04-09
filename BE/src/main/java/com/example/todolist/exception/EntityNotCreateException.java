90c819fc8be7ba610e8802f3f4da936f5c6e78de
jeong-inho <derosatam76@gmail.com>
2021-04-09 21:38:55 +0900

    Feat : Exception 클래스에 상태코드 정의
    EntityNotCreateException 클래스에 404코드, UserAccountException 클래스에 200코드 부여

--- BE/src/main/java/com/example/todolist/exception/EntityNotCreateException.java
+++ BE/src/main/java/com/example/todolist/exception/EntityNotCreateException.java
@@ -1,5 +1,9 @@
 package com.example.todolist.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class EntityNotCreateException extends RuntimeException {
 
     public EntityNotCreateException() {
--- BE/src/main/java/com/example/todolist/exception/UserAccountException.java
+++ BE/src/main/java/com/example/todolist/exception/UserAccountException.java
@@ -1,11 +1,15 @@
 package com.example.todolist.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.OK)
 public class UserAccountException extends RuntimeException {
 
     private ErrorMessage errorMessage;
 
     public UserAccountException(ErrorMessage errorMessage) {
-        super(ErrorMessage.LOGIN_FAILED.getErrorMessage());
+        super(errorMessage.getErrorMessage());
         this.errorMessage = errorMessage;
     }
 
