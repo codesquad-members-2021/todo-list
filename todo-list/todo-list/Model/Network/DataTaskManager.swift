68f6456629d4289bd7ece9ce013b031af6f01f7e
Sonjh1306 <yale1306@gmail.com>
2021-04-09 14:48:02 +0900

    feat: Parsing을 담당하는 ParsingManager 구현

---
+++ todo-list/todo-list/Model/Network/DataTaskManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataTaskManager.swift
+//  todo-list
+//
+//  Created by sonjuhyeong on 2021/04/09.
+//
+
+import Foundation
---
+++ todo-list/todo-list/Model/Network/RequestManager.swift
@@ -0,0 +1,8 @@
+//
+//  RequestManager.swift
+//  todo-list
+//
+//  Created by sonjuhyeong on 2021/04/09.
+//
+
+import Foundation
