40f2524852f9fd18f09c1a229ca5efe1096ed71a
정이삭 <isaac56@naver.com>
2021-04-09 12:58:14 +0900

    [#23] chore: Change schema
    Spring-Data-JDBC에서는 Many to One을 지원하지 않습니다.
    따라서 column_type, action_type 등의 테이블을 제거하고 외래키 대신 문자열을 칼럼에 직접 넣게 하였습니다.

--- BE/todo/src/main/resources/data.sql
+++ BE/todo/src/main/resources/data.sql
@@ -1,1 +1,7 @@
-;
+INSERT INTO `user` (`user_id`,`password`) VALUES ('noel', '1234');
+INSERT INTO `card` (`user`, `title`, `content`, `priority`, `column_type`) VALUES (1, '제목입니다.', '내용입니다.', 1, '할일');
+INSERT INTO `card` (`USER`, `TITLE`, `CONTENT`, `PRIORITY`, `COLUMN_TYPE`) VALUES (1, '테스트1', '내용', 1.3, '하는중');
+INSERT INTO `card` (`USER`, `TITLE`, `CONTENT`, `PRIORITY`, `COLUMN_TYPE`) VALUES (1, '테스트2', '내용', 1.6, '완료');
+INSERT INTO `history` (`CARD`, `ACTION`, `DATE`, `FROM`, `TO`) VALUES ('1', '이동', now(), '할일', '하는중');
+INSERT INTO `history` (`CARD`, `ACTION`, `DATE`, `FROM`, `TO`) VALUES ('2', '추가', now(), null, '하는중');
+INSERT INTO `history` (`CARD`, `ACTION`, `DATE`, `FROM`, `TO`) VALUES ('2', '삭제', now(), '할일', null);
