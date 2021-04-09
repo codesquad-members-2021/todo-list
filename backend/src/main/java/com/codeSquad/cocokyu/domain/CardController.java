15254cb31d794fe8bbf7ee0eb547dd6fe355651c
gisungchoi <chlrltjd159@naver.com>
2021-04-09 10:56:14 +0900

    feat : LogList 작성
    - json으로 반환시 LogList 객체에 담아서 제공합니다.

--- backend/src/main/java/com/codeSquad/cocokyu/domain/CardController.java
+++ backend/src/main/java/com/codeSquad/cocokyu/domain/CardController.java
@@ -1,11 +1,14 @@
 package com.codeSquad.cocokyu.domain;
 
-import com.codeSquad.cocokyu.domain.card.Card;
+import com.codeSquad.cocokyu.domain.model.Card;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CardController {
 
+    Logger logger = LoggerFactory.getLogger(CardController.class);
     private CardService cardService;
 
     public CardController(CardService cardService) {
@@ -17,8 +20,14 @@ public class CardController {
         return cardService.sortedList();
     }
 
+    @GetMapping("/logs")
+    public LogList logList() {
+        return cardService.getList();
+    }
+
     @PostMapping("/todos")
     public String create(Card card) {
+        logger.info(card.toString());
         cardService.write(card);
         //TODO : Response 객체 생성
         return "success";
@@ -31,7 +40,7 @@ public class CardController {
     }
 
     @DeleteMapping("/todos/{id}")
-    public String delete(@PathVariable Long id){
+    public String delete(@PathVariable Long id) {
         cardService.delete(id);
         return "success";
     }
