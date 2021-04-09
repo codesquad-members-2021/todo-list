ac48171db49f15830bbe3c9f7f2f3b4f87596966
Jaeuk-YOO <ryou9305@naver.com>
2021-04-09 23:57:44 +0900

    fix: TodoColumn으로 넘겨주는 값 구조 수정

--- src/Components/TodoColumnList/TodoColumnListContainer.js
+++ src/Components/TodoColumnList/TodoColumnListContainer.js
@@ -1,22 +1,29 @@
 import React, {useState, useEffect } from 'react';
+import styled from 'styled-components';
+
 import TodoColumnContainer from '../TodoColumn/TodoColumnContainer.js';
 
 const TodoColumnListContainer = ({ handleChangeTodo }) => {
-  const [columns, setColumns] = useState(['schedule', 'progress', 'complete']);
+  const [columns, setColumns] = useState([
+    {title: 'schedule'},{title: 'progress'},{title: 'complete'}
+  ]);
   
 
   useEffect(() => {
     // 서버로부터 컬럼 정보를 받는다. => setColumns 변환
-    console.log("TodoColumnListContainer");
   }, []);
   
   return (
-    <div>
+    <TodoColumnContainerLayout>
       {columns.map((colName, i) => {
         return <TodoColumnContainer state={colName} key={`${colName}-${i}`} />;
       })}      
-    </div>
+    </TodoColumnContainerLayout>
   )
 }
+
+const TodoColumnContainerLayout = styled.div`
+  display: flex;
+`
 
 export default TodoColumnListContainer;
