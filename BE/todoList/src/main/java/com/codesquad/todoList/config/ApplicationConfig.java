529823bdd007c25a766d034cf67738184134f1ca
tmdgusya <tmdgusya@naver.com>
2021-04-09 22:09:44 +0900

    [#31] refactor: :recycle: 필요없는 Config 옵션제거

--- BE/todoList/src/main/java/com/codesquad/todoList/config/ApplicationConfig.java
+++ BE/todoList/src/main/java/com/codesquad/todoList/config/ApplicationConfig.java
@@ -15,17 +15,7 @@ import org.springframework.transaction.TransactionManager;
 import javax.sql.DataSource;
 
 @Configuration
-@EnableJdbcRepositories
 public class ApplicationConfig extends AbstractJdbcConfiguration {
 
-    @Bean
-    NamedParameterJdbcOperations namedParameterJdbcOperations(DataSource dataSource) {
-        return new NamedParameterJdbcTemplate(dataSource);
-    }
-
-    @Bean
-    TransactionManager transactionManager(DataSource dataSource) {
-        return new DataSourceTransactionManager(dataSource);
-    }
 
 }
