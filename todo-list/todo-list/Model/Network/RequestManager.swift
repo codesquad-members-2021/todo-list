11eb5d4c099887e18bd0a442202e4939c266b1b2
Sonjh1306 <yale1306@gmail.com>
2021-04-09 14:49:54 +0900

    feat: Network 요청을 담당하는 RequestManager 구현

--- todo-list/todo-list/Model/Network/RequestManager.swift
+++ todo-list/todo-list/Model/Network/RequestManager.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+
+class RequestManager {
+        
+    //GET
+    static func get(url: String) -> URLRequest {
+        guard let url = URL(string: url) else {
+            print("The URL is inappropriate.")
+            return URLRequest(url: URL(string: "")!)
+        }
+        return URLRequest(url: url)
+    }
+        
+    //POST
+    static func post(url: String, data: Data) -> URLRequest {
+        guard let url = URL(string: url) else {
+            print("The URL is inappropriate.")
+            return URLRequest(url: URL(string: "")!)
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = data
+//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        return request
+    }
+    
+    //delete
+    static func delete(url: String) -> URLRequest {
+        guard let url = URL(string: url) else {
+            print("The URL is inappropriate.")
+            return URLRequest(url: URL(string: "")!)
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        return request
+    }
+    
+}
+
+class NetworkManager {
+    
+    static func getData(urlString: String, completion: @escaping (String) -> ()) {
+        if let url = URL(string: urlString) {
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let data = data {
+                    guard let result = ParsingManager.decodeData(type: TodoInfoList.self, data: data) else {
+                        return
+                    }
+                    print(result)
+                    completion(result.getTodoListString())
+                }
+            }.resume()
+        }
+    }
+}
