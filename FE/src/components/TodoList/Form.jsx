63a2f51c0a64a4b9a06dd1d3a78239879683c71e
SEONKYU\difke <somedaycode@gmail.com>
2021-04-09 16:38:06 +0900

    [Add]: Form을 구성하는 코드 설계

--- FE/src/components/TodoList/Form.jsx
+++ FE/src/components/TodoList/Form.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
 
 
-const Form = ({isClicked}) => {
-
+const Form = ({isClicked, cardList, addCard ,id, toggleForm}) => {
     const addTodo = (e) => {
         e.preventDefault();
     }
 
     const NewCard = () => (
-    <form action = "">
+    <form action="">
     <input type="text" placeholder="제목을 적어주세요"></input>
     <input type="text" placeholder="내용을 적어주세요"></input>
     <div>
     <button onClick={addTodo}>등록</button>
     <button onClick={addTodo}>취소</button>
-    </div>
+    </div> 
     </form>);
     return (
         <>
-        {isClicked ? (<NewCard/>) : (<></>)}
+        <NewCard/>
     </>
     )
 }
