1b4f4d3b3a8dea163f021bd8a88c6c1df8016807
jeong-inho <derosatam76@gmail.com>
2021-04-10 14:19:25 +0900

    Feat : TimelineController 클래스에 showTimeline 메소드 수정
    기존에 정적으로 timeline의 더미 데이터만 반환하던 로직에서 동적으로 sessionUser를 확인해 반환하도록 수정

--- BE/src/main/java/com/example/todolist/web/TimelineController.java
+++ BE/src/main/java/com/example/todolist/web/TimelineController.java
@@ -3,16 +3,20 @@ package com.example.todolist.web;
 import com.example.todolist.domain.timeline.Timeline;
 import com.example.todolist.domain.timeline.TimelineRepository;
 import com.example.todolist.domain.user.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
 import java.util.List;
+
+import static com.example.todolist.web.utils.HttpSessionUtils.getUserFromSession;
 
 @RestController
 public class TimelineController {
 
+    private final Logger logger = LoggerFactory.getLogger(TimelineController.class);
     public final TimelineRepository timelineRepository;
 
     public TimelineController(TimelineRepository timelineRepository) {
@@ -21,11 +25,9 @@ public class TimelineController {
 
     @GetMapping("/timelines")
     public List<Timeline> showTimeline(HttpSession session) {
-        User user = (User) session.getAttribute("sessionUser");
-        if (user == null || user.getId() != 1) {
-            return new ArrayList<>();
-        }
-        return timelineRepository.findAllByAuthor(1L);
+        logger.info("타임라인 리스트 요청");
+        User sessionUser = getUserFromSession(session);
+        return timelineRepository.findAllByAuthor(sessionUser.getId());
     }
 
 }
