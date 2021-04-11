bde663b2ca9f389ad7ab69898af74aea17b738f4
lena <sue822330@gmail.com>
2021-04-11 17:27:25 +0900

    feat: context menu 완료된 일로 이동시키기 UI 기능 추가

--- TodoList/TodoList/Do/DoViewController.swift
+++ TodoList/TodoList/Do/DoViewController.swift
@@ -22,7 +22,7 @@ class DoViewController: UIViewController {
     
     func configure() {
         doTableView.dataSource = dataSource
-        doTableView.delegate = delegate
+        doTableView.delegate = self
         
         doTableView.estimatedRowHeight = 108
         doTableView.rowHeight = UITableView.automaticDimension
@@ -38,4 +38,45 @@ class DoViewController: UIViewController {
     }
 }
 
+extension DoViewController : UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        let cellSpacingHeight: CGFloat = 15
+        return cellSpacingHeight
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = .clear
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+       if editingStyle == .delete {
+       }
+    }
+    
+    func tableView(_ tableView: UITableView, contextMenuConfigurationForRowAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+        return UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { suggestedActions in
+            
+            let share = UIAction(title: "완료한 일로 이동") { action in
+                let task = self.dataSource.doDTO.move(index: indexPath.section)
+                NotificationCenter.default.post(name: .taskCompleted, object: self, userInfo: ["task": task])
+            }
+            
+            let rename = UIAction(title: "수정하기") { action in
+                // Perform renaming
+            }
+            
+            let delete = UIAction(title: "삭제하기", attributes: .destructive) { action in
+                // Perform delete
+            }
+            
+            return UIMenu(title: "", children: [share, rename, delete])
+        }
+    }
+}
 
+extension Notification.Name {
+    static let taskCompleted = Notification.Name(rawValue: "taskCompleted")
+}
