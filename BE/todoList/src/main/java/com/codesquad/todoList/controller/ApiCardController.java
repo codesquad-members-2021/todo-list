b98bdf407c6295e6fe54d521fe42376ab1fb887f
tmdgusya <tmdgusya@naver.com>
2021-04-11 13:39:44 +0900

    [#33] feat: :hamster: ErrorResponse Return Code 추가

--- BE/todoList/src/main/java/com/codesquad/todoList/controller/ApiCardController.java
+++ BE/todoList/src/main/java/com/codesquad/todoList/controller/ApiCardController.java
@@ -1,10 +1,13 @@
 package com.codesquad.todoList.controller;
 
 import com.codesquad.todoList.entity.Card;
+import com.codesquad.todoList.error.ErrorCode;
+import com.codesquad.todoList.error.ErrorResponse;
 import com.codesquad.todoList.service.ColumnService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -22,8 +25,9 @@ public class ApiCardController {
     @PostMapping()
     public ResponseEntity<?> addCard(@Validated @RequestBody Card card, @PathVariable Long id, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) {
-            //TODO ERROR MESSAGE PUT RESPONSE BODY
-            log.error("ERROR! ");
+            ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.BAD_REQUEST, bindingResult);
+            log.error("ADD CARD ERROR : {}", errorResponse);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
         }
         columnService.addCard(id, card);
         return ResponseEntity.ok().body(card);
@@ -32,15 +36,16 @@ public class ApiCardController {
 
     @DeleteMapping("/{cardId}")
     public ResponseEntity<?> deleteCard(@PathVariable Long id, @PathVariable Long cardId) {
-        boolean jobSuccessed = columnService.delete(id, cardId);
-        return ResponseEntity.ok().body(jobSuccessed);
+        boolean jobSucceed = columnService.delete(id, cardId);
+        return ResponseEntity.ok().body(jobSucceed);
     }
 
     @PutMapping("/{cardId}")
     public ResponseEntity<?> updateCard(@PathVariable Long id, @PathVariable Long cardId, @Validated @RequestBody Card card, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) {
-            //TODO ERROR MESSAGE PUT RESPONSE BODY
-            log.error("UPDATE CARD ERROR");
+            ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.BAD_REQUEST, bindingResult);
+            log.error("UPDATE CARD ERROR : {}", errorResponse);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
         }
         Card updateCard = columnService.updateCard(id, cardId, card);
         return ResponseEntity.ok(updateCard);
