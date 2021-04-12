7f2c5f3fc2b40aa7dff4e0f3a39035e192aacc3a
BMO <b38238496@gmail.com>
2021-04-12 16:40:05 +0900

    [#4] feat: 카드추가 ModalViewController 구현
    - button tapped 시 화면 중앙에 새로운 카드 작성 화면 구현
    - place holder 적용
    - 취소, 등록 Button 생성
    - 글자수 500자 제한
    - 글의 길이에 맞춰 박스 길이 조정

--- ToDoListApp/ToDoListApp/Controller/ModalViewController.swift
+++ ToDoListApp/ToDoListApp/Controller/ModalViewController.swift
@@ -12,27 +12,64 @@ class ModalViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentTextView: UITextView!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var updateButton: UIButton!
+    
+    @IBOutlet weak var backgroundViewHeigthConstraint: NSLayoutConstraint!
+    
+    private var contentTextViewDelegate = ModalTextViewDelegate()
+    private var sizeManager = SizeManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(changeSize), name: SizeManager.changeSize, object: sizeManager)
+        
         setUpView()
+        setUpButton()
+        setUpDelegate()
+        setUpViewSize()
     }
     
     func setUpView() {
         let cornerRadius: CGFloat = 5
         self.view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
         self.backgroundView.layer.cornerRadius = cornerRadius
+        
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUpButton() {
+        let cornerRadius: CGFloat = 5
+        self.cancelButton.layer.cornerRadius = cornerRadius
+        self.updateButton.layer.cornerRadius = cornerRadius
     }
-    */
+    
+    func setUpDelegate() {
+        self.contentTextView.delegate = contentTextViewDelegate
+    }
+    
+    func setUpViewSize() {
+        let contentTextViewHeight = contentTextView.sizeThatFits(contentTextView.contentSize).height
+        self.sizeManager.setInitialHeight(viewHeight: backgroundViewHeigthConstraint.constant, contentTextViewHeight: contentTextViewHeight)
+        
+        self.contentTextViewDelegate.configureHeight { height in
+            self.sizeManager.changeHeight(height)
+        }
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
 
+    @objc func changeSize(notification: Notification) {
+        guard let backgroundViewHeight = notification.userInfo?[SizeManager.Size.backgroundViewHeight] as? CGFloat
+        else {
+            return
+        }
+        
+        self.backgroundViewHeigthConstraint.constant = backgroundViewHeight
+    }
+    
 }
