51092ae2a903d7182c96a95158b188934dd40f41
95degree <95degree@naver.com>
2021-04-12 15:08:51 +0900

    feat : create() 추가
    -BoardDto를 받아 테이블에 추가하기 위해 create()추가

--- backend/todolist/src/main/java/team10/todolist/controller/BoardController.java
+++ backend/todolist/src/main/java/team10/todolist/controller/BoardController.java
@@ -1,4 +1,27 @@
 package team10.todolist.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team10.todolist.domain.Board;
+import team10.todolist.dto.BoardDto;
+import team10.todolist.repository.BoardRepository;
+
+@RestController
+@RequestMapping("/board")
 public class BoardController {
+
+    private final BoardRepository boardRepository;
+
+    public BoardController(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    @PostMapping("/lists")
+    public Board create(@RequestBody BoardDto boardDto) {
+        Board board = boardDto.toEntity();
+        //cardRepository.save(card);
+        return board;
+    }
 }
