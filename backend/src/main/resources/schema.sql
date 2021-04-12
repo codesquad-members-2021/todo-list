3e9bcbe47a91f93ac99c0c50e73da3abe05bc27c
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-04-12 22:37:34 +0900

    feat: Delete schema.sql file

--- backend/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists card;
-
-create table card (
-        id bigint auto_increment primary key,
-        title varchar(50),
-        content varchar(500),
-        status varchar(50),
-        created_time timestamp
-);
