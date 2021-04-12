1235763acaac0a90d5df6c28f1f40fcd53bbd547
Dami Kim <dami.k.dev@gmail.com>
2021-04-12 17:43:42 +0900

    Feat: App.js style component 추가

--- todo/src/components/pages/App.js
+++ todo/src/components/pages/App.js
@@ -1,14 +1,19 @@
-import "./App.scss";
 import { useState, useEffect } from "react";
 import useToggle from "../../hooks/useToggle";
 import axios from "axios";
 import Header from "../molecules/Header";
 import HistoryContent from "../organisms/HistoryContent";
 import TodoContent from "../organisms/TodoContent";
+import styled from "styled-components";
 const getAxios = async (setState, url) => {
   const { data } = await axios.get(url);
   setState(data);
 };
+const Div = styled.div`
+  border: 3px solid black;
+  background: #63bda4;
+  border-radius: 10px;
+`;
 
 function App() {
   const [isOpen, isOpenActions] = useToggle(false);
@@ -26,11 +31,11 @@ function App() {
   }, [todoTitle]);
 
   return (
-    <div className="App">
+    <Div>
       <HistoryContent isOpen={isOpen} isOpenActions={isOpenActions} />
       <Header isOpenActions={isOpenActions} />
       <TodoContent></TodoContent>
-    </div>
+    </Div>
   );
 }
 
