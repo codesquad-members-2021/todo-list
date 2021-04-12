be154b4ebc1d72bbc7d83c58c854bb49f680c5db
95degree <95degree@naver.com>
2021-04-12 15:30:57 +0900

    feat : BoardController 로직 변경
    -repository가 아닌 service를 의존성 주입을 받도록 변경

--- backend/todolist/src/main/java/team10/todolist/controller/BoardController.java
+++ backend/todolist/src/main/java/team10/todolist/controller/BoardController.java
@@ -4,24 +4,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import team10.todolist.domain.Board;
 import team10.todolist.dto.BoardDto;
-import team10.todolist.repository.BoardRepository;
+import team10.todolist.service.BoardService;
 
 @RestController
 @RequestMapping("/board")
 public class BoardController {
 
-    private final BoardRepository boardRepository;
+    private final BoardService boardService;
 
-    public BoardController(BoardRepository boardRepository) {
-        this.boardRepository = boardRepository;
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
     }
 
     @PostMapping("/lists")
-    public Board create(@RequestBody BoardDto boardDto) {
-        Board board = boardDto.toEntity();
-        //cardRepository.save(card);
-        return board;
+    public boolean create(@RequestBody BoardDto boardDto) {
+        return boardService.create(boardDto);
     }
 }
--- backend/todolist/src/main/java/team10/todolist/service/BoardService.java
+++ backend/todolist/src/main/java/team10/todolist/service/BoardService.java
@@ -1,6 +1,8 @@
 package team10.todolist.service;
 
 import org.springframework.stereotype.Service;
+import team10.todolist.domain.Board;
+import team10.todolist.dto.BoardDto;
 import team10.todolist.repository.BoardRepository;
 
 @Service
@@ -10,5 +12,11 @@ public class BoardService {
 
     public BoardService(BoardRepository boardRepository){
         this.boardRepository = boardRepository;
+    }
+
+    public boolean create(BoardDto boardDto){
+        Board board = boardDto.toEntity();
+        boardRepository.save(board);
+        return true;
     }
 }
