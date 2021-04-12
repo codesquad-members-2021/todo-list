b5f515d7b0d1458a47ca3b728447b693aa46bd66
jeong-inho <derosatam76@gmail.com>
2021-04-12 18:34:26 +0900

    Refactor : 불필요한 어노테이션 삭제

--- BE/src/main/java/com/example/todolist/domain/timeline/TimelineRepository.java
+++ BE/src/main/java/com/example/todolist/domain/timeline/TimelineRepository.java
@@ -1,11 +1,9 @@
 package com.example.todolist.domain.timeline;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface TimelineRepository extends CrudRepository<Timeline, Long> {
 
     List<Timeline> findAllByAuthor(Long author);
