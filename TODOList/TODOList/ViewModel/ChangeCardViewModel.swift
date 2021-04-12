102ed9503201c1d72130300434b317e2ffcedf28
okstring <milkoi7801@gmail.com>
2021-04-12 19:08:28 +0900

    Implement: Card Add 흐름 구현

--- TODOList/TODOList/ViewModel/ChangeCardViewModel.swift
+++ TODOList/TODOList/ViewModel/ChangeCardViewModel.swift
@@ -10,26 +10,36 @@ import Foundation
 class ChangeCardViewModel {
     private(set) var subject: Observable<String>
     private(set) var body: Observable<String>
-    private 
+    private var cardsNetworkCenter: CardsNetworkCenter
+    
+    var addCardClosure: ((Card) -> Void)?
     
     init(subject: String, body: String) {
         self.subject = Observable(value: subject)
         self.body = Observable(value: body)
+        self.cardsNetworkCenter = CardsNetworkCenter()
     }
     
     convenience init() {
         self.init(subject: "", body: "")
+        self.cardsNetworkCenter = CardsNetworkCenter()
     }
     
     func addCard(mode: SectionMode) {
         guard let title = subject.value else { return }
         guard let contents = body.value else { return }
         let card = CardFactory.makeCard(title: title, contents: contents, mode: mode)
-        
+        self.cardsNetworkCenter.postCards(card: card) { (card) in
+            self.addCardClosure?(card)
+        }
     }
     
     func deleteCard(card: Card) {
         //delete
+    }
+    
+    func modifyCard(card: Card) {
+        //modify
     }
     
 }
