21e00b00480fd8807dd130785781b752a2f7cff9
lena <sue822330@gmail.com>
2021-04-12 14:36:35 +0900

    feat: 활동 기록 UI 추가

--- TodoList/TodoList/ViewController.swift
+++ TodoList/TodoList/ViewController.swift
@@ -9,10 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var activityView: UIView!
+    @IBOutlet weak var activityTrailingConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        activityTrailingConstraint.constant -= activityView.frame.width
     }
 
-
+    
+    @IBAction func touchUpMenu(_ sender: Any) {
+        activityTrailingConstraint.constant = 0
+        UIView.animate(withDuration: 0.4) { [weak self] in
+            self?.view.layoutIfNeeded()
+        }
+    }
+    
+    @IBAction func touchUpClose(_ sender: Any) {
+        activityTrailingConstraint.constant -= activityView.frame.width
+        UIView.animate(withDuration: 0.4) { [weak self] in
+            self?.view.layoutIfNeeded()
+        }
+    }
+    
 }
 
