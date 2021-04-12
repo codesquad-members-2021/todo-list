bfffb101290a2decd9d266c443302de93e6a73ce
95degree <95degree@naver.com>
2021-04-12 14:58:10 +0900

    feat : CardDto에 toEntity() 추가
    -Dto에 담긴 정보들을 Entity로 만들어 주기 위해 toEntity()추가

--- backend/todolist/src/main/java/team10/todolist/dto/CardDto.java
+++ backend/todolist/src/main/java/team10/todolist/dto/CardDto.java
@@ -1,4 +1,34 @@
 package team10.todolist.dto;
 
+import team10.todolist.domain.Card;
+
 public class CardDto {
+    private String title;
+    private String contents;
+    private String category;
+
+    protected CardDto() {
+    }
+
+    public CardDto(String title, String contents, String category) {
+        this.title = title;
+        this.contents = contents;
+        this.category = category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Card toEntity() {
+        return new Card(title, contents, category);
+    }
 }
