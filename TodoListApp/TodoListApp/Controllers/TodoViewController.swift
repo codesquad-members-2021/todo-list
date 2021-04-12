831ef548511c18171240c58970ce1d583eb85af0
1song2 <1song2918@gmail.com>
2021-04-12 17:29:53 +0900

    Add section headers to tableView for spacing

--- TodoListApp/TodoListApp/Controllers/TodoViewController.swift
+++ TodoListApp/TodoListApp/Controllers/TodoViewController.swift
@@ -20,6 +20,7 @@ class TodoViewController: UIViewController, ListViewControllerProtocol {
         headerView.badgeButton.layer.cornerRadius = headerView.frame.size.height / 2
         
         cardTableView.dataSource = self
+        cardTableView.delegate  = self
         cardTableView.register(CardCell.nib(), forCellReuseIdentifier: CardCell.identifier)
     }
     
@@ -35,16 +36,24 @@ class TodoViewController: UIViewController, ListViewControllerProtocol {
     }
 }
 
-extension TodoViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+extension TodoViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return cards.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 16.0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell  = tableView.dequeueReusableCell(withIdentifier: CardCell.identifier, for: indexPath) as! CardCell
         DispatchQueue.main.async {
-            cell.titleLabel.text = self.cards[indexPath.row].title
-            cell.descriptionLabel.text = self.cards[indexPath.row].description
+            cell.titleLabel.text = self.cards[indexPath.section].title
+            cell.descriptionLabel.text = self.cards[indexPath.section].description
         }
         return cell
     }
