7d872601ab67905f525a925cb2a86d9a4f4a6174
95degree <95degree@naver.com>
2021-04-12 15:46:48 +0900

    feat : readAll() 생성
    - 모든 borad를 가져오기 위해 readall() 생성

--- backend/todolist/src/main/java/team10/todolist/controller/BoardController.java
+++ backend/todolist/src/main/java/team10/todolist/controller/BoardController.java
@@ -1,11 +1,11 @@
 package team10.todolist.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import team10.todolist.domain.Board;
 import team10.todolist.dto.BoardDto;
 import team10.todolist.service.BoardService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/board")
@@ -21,4 +21,10 @@ public class BoardController {
     public boolean create(@RequestBody BoardDto boardDto) {
         return boardService.create(boardDto);
     }
+
+    @GetMapping("/lists")
+    public List<Board> readAll(){
+        return boardService.readAll();
+    }
+
 }
--- backend/todolist/src/main/java/team10/todolist/service/BoardService.java
+++ backend/todolist/src/main/java/team10/todolist/service/BoardService.java
@@ -5,6 +5,9 @@ import team10.todolist.domain.Board;
 import team10.todolist.dto.BoardDto;
 import team10.todolist.repository.BoardRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class BoardService {
 
@@ -18,5 +21,12 @@ public class BoardService {
         Board board = boardDto.toEntity();
         boardRepository.save(board);
         return true;
+    }
+
+    public List<Board> readAll(){
+        Iterable<Board> boardIterable = boardRepository.findAll();
+        List<Board> boards = new ArrayList<>();
+        boardIterable.forEach(boards::add);
+        return boards;
     }
 }
