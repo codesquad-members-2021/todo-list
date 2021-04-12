29bed87c5445b7aacc7a1619b8af57c369b7b053
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-04-12 15:12:43 +0900

    feat : 컨트롤러의 응답객체를 ResponseEntity로 수정

--- be/todo/src/main/java/com/codesquad/todo/web/controller/ColumnController.java
+++ be/todo/src/main/java/com/codesquad/todo/web/controller/ColumnController.java
@@ -4,6 +4,7 @@ import com.codesquad.todo.web.domain.Column;
 import com.codesquad.todo.web.domain.Task;
 import com.codesquad.todo.web.domain.User;
 import com.codesquad.todo.web.service.dto.ColumnDto;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ import java.util.Map;
 public class ColumnController {
 
     @GetMapping
-    public Map<String, Object> showColumnList() {
+    public ResponseEntity<Map<String, Object>> showColumnList() {
         Map<String, Object> responseMap = new HashMap<>();
 
         List<ColumnDto> columns = new ArrayList<>();
@@ -35,7 +36,7 @@ public class ColumnController {
 
 
         responseMap.put("columns", columns);
-        return responseMap;
+        return ResponseEntity.ok(responseMap);
     }
 
 }
--- be/todo/src/main/java/com/codesquad/todo/web/controller/TodoLogController.java
+++ be/todo/src/main/java/com/codesquad/todo/web/controller/TodoLogController.java
@@ -3,6 +3,7 @@ package com.codesquad.todo.web.controller;
 import com.codesquad.todo.web.domain.TodoLog;
 import com.codesquad.todo.web.domain.User;
 import com.codesquad.todo.web.service.dto.TodoLogDto;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ import java.util.Map;
 public class TodoLogController {
 
     @GetMapping
-    public Map<String, Object> showTodoLogList() {
+    public ResponseEntity<Map<String, Object>> showTodoLogList() {
         Map<String, Object> responseMap = new HashMap<>();
         List<TodoLogDto> todoLogs = new ArrayList<>();
 
@@ -26,6 +27,6 @@ public class TodoLogController {
         todoLogs.add(new TodoLogDto(new TodoLog(2L, "'move' '2 자바스크립트 예제 실습 수정됨!' 'TODO' 'IN_PROGRESS'", author)));
 
         responseMap.put("todoLogs", todoLogs);
-        return responseMap;
+        return ResponseEntity.ok(responseMap);
     }
 }
