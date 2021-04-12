83894fef2760e3624a18ddfe799770cb5450a42a
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-04-12 19:04:50 +0900

    fix : 기능 변경으로 인해 일시적으로 목업 컨트롤러 주석처리

--- be/todo/src/main/java/com/codesquad/todo/web/controller/ColumnController.java
+++ be/todo/src/main/java/com/codesquad/todo/web/controller/ColumnController.java
@@ -18,25 +18,27 @@ import java.util.Map;
 @RequestMapping("/api/columns")
 public class ColumnController {
 
+/*
     @GetMapping
     public ResponseEntity<Map<String, Object>> showColumnList() {
         Map<String, Object> responseMap = new HashMap<>();
 
         List<ColumnDto> columns = new ArrayList<>();
-        User author = new User(1L, "라쿠운", "Racoon", "1234", "");
+        User author = new User("라쿠운", "Racoon", "1234", "");
 
-        Column todoColumn = new Column(1L, "TODO");
-        todoColumn.addTask(new Task(1L, "task1", "taskContent1", todoColumn));
-        todoColumn.addTask(new Task(2L, "task2", "taskContent2", todoColumn));
+        Column todoColumn = new Column("TODO");
+        todoColumn.addTask(new Task("task1", "taskContent1"));
+        todoColumn.addTask(new Task("task2", "taskContent2"));
         columns.add(new ColumnDto(todoColumn));
 
-        Column inProgressColumn = new Column(2L, "IN_PROGRESS");
-        inProgressColumn.addTask(new Task(3L, "task3", "taskContent3", inProgressColumn));
+        Column inProgressColumn = new Column("IN_PROGRESS");
+        inProgressColumn.addTask(new Task("task3", "taskContent3"));
         columns.add(new ColumnDto(inProgressColumn));
 
 
         responseMap.put("columns", columns);
         return ResponseEntity.ok(responseMap);
     }
+*/
 
 }
--- be/todo/src/main/java/com/codesquad/todo/web/controller/TodoLogController.java
+++ be/todo/src/main/java/com/codesquad/todo/web/controller/TodoLogController.java
@@ -17,16 +17,16 @@ import java.util.Map;
 @RequestMapping("/api/logs")
 public class TodoLogController {
 
-    @GetMapping
+    /*@GetMapping
     public ResponseEntity<Map<String, Object>> showTodoLogList() {
         Map<String, Object> responseMap = new HashMap<>();
         List<TodoLogDto> todoLogs = new ArrayList<>();
 
-        User author = new User(1L, "라쿠운", "Racoon", "1234", "");
-        todoLogs.add(new TodoLogDto(new TodoLog(1L, "'move' '1 자바스크립트 예제 실습 수정됨!' 'TODO' 'IN_PROGRESS'", author)));
-        todoLogs.add(new TodoLogDto(new TodoLog(2L, "'move' '2 자바스크립트 예제 실습 수정됨!' 'TODO' 'IN_PROGRESS'", author)));
+        User author = new User("라쿠운", "Racoon", "1234", "");
+        todoLogs.add(new TodoLogDto(new TodoLog("'move' '1 자바스크립트 예제 실습 수정됨!' 'TODO' 'IN_PROGRESS'", author)));
+        todoLogs.add(new TodoLogDto(new TodoLog("'move' '2 자바스크립트 예제 실습 수정됨!' 'TODO' 'IN_PROGRESS'", author)));
 
         responseMap.put("todoLogs", todoLogs);
         return ResponseEntity.ok(responseMap);
-    }
+    }*/
 }
