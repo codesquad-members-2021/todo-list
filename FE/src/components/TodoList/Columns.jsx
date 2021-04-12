2dd0b7912f967860eae2ad56def46a4502ded77a
SEONKYU\difke <somedaycode@gmail.com>
2021-04-12 17:29:22 +0900

    [Modify]: prettier적용으로 인해 코드 라인이 바뀌었다

--- FE/src/components/TodoList/Columns.jsx
+++ FE/src/components/TodoList/Columns.jsx
@@ -1,62 +1,70 @@
-import React, {useState, useEffect} from 'react';
-import {ColumnHeader, ColumnContainer} from './columnStyle'
-import {ColumnTitle, ColumnIcon} from '../atoms/ColumnHeader'
-import Card from '../atoms/Card'
-import Form from './Form'
+import React, { useState, useEffect } from 'react';
+import { ColumnHeader, ColumnContainer } from './columnStyle';
+import { ColumnTitle, ColumnIcon } from '../atoms/ColumnHeader';
+import Card from '../atoms/Card';
+import Form from './Form';
 
+const Column = ({ data: { columns } }) => {
+  const [initialColumns, setColumn] = useState(columns);
+  const [currentID, setCurrentID] = useState(null);
 
-const Column = ({data:{columns}}) => {
-    const [initialColumns, setColumn] = useState(columns);
-    const [currentID, setCurrentID] = useState(null);
-
-
-
-    const handleClick = (clickedID) => {
-        return () => {
-            if(currentID !== clickedID) setCurrentID(clickedID);
-            else if(currentID !== null) setCurrentID(null);
-            else setCurrentID(clickedID)
-        };
+  const handleClick = (clickedID) => {
+    return () => {
+      if (currentID !== clickedID) setCurrentID(clickedID);
+      else if (currentID !== null) setCurrentID(null);
+      else setCurrentID(clickedID);
     };
+  };
 
-    const deleteColumn = (deletedID) => {
-        return () => {
-            const newColumn = initialColumns.filter(({id}) => id !== deletedID)
-            setColumn([...newColumn])
-        }
-    }
+  const deleteColumn = (deletedID) => {
+    return () => {
+      const newColumn = initialColumns.filter(({ id }) => id !== deletedID);
+      setColumn([...newColumn]);
+    };
+  };
 
-    const addCard = (column) => {
-        const newColumns = Object.assign(initialColumns, column)
-        setColumn([...newColumns]);
-    }
+  const addCard = (column) => {
+    const newColumns = Object.assign(initialColumns, column);
+    setColumn([...newColumns]);
+  };
 
-    const handleClickCancel = () => {
-        setCurrentID(null);
-    }
-    const columnList = initialColumns.map((column) => {
-        const {id, title, cards} = column;
-        return (<ul key={id}>
-        <header>
-        <span>{title}</span>
-        <span>{cards.length}개</span>
-        <button onClick={handleClick(id)}><i className="fas fa-plus"></i></button>
-        <button onClick={deleteColumn(id)}><i className="fas fa-times"></i></button>
-        </header>
-        <div>
-        
-        {currentID === id ? <Form key={id} addCard={addCard} handleClickCancel={handleClickCancel} column={column}/> : <></>}
-        <Card cards={cards} />
-
-        </div>
-        </ul>)
-        })
-
+  const handleClickCancel = () => {
+    setCurrentID(null);
+  };
+  const columnList = initialColumns.map((column) => {
+    const { id, title, cards } = column;
     return (
-        <ColumnContainer>
-        {columnList}
-        </ColumnContainer>
-    )
-}
+      <section key={id}>
+        <ul>
+          <header>
+            <span>{title}</span>
+            <span>{cards.length}개</span>
+            <button onClick={handleClick(id)}>
+              <i className="fas fa-plus"></i>
+            </button>
+            <button onClick={deleteColumn(id)}>
+              <i className="fas fa-times"></i>
+            </button>
+          </header>
+          <div>
+            {currentID === id ? (
+              <Form
+                key={id}
+                addCard={addCard}
+                handleClickCancel={handleClickCancel}
+                column={column}
+              />
+            ) : (
+              <></>
+            )}
+            <Card cards={cards} />
+          </div>
+        </ul>
+      </section>
+    );
+  });
 
-export default Column
+  return <ColumnContainer>{columnList}</ColumnContainer>;
+};
+
+export default Column;
