5ced4c48424e9baefdbc08942a11173c4b58c249
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-04-12 23:35:38 +0900

    feat: Change logging level

--- backend/src/main/java/com/codesquad/todolist/TodolistApplication.java
+++ backend/src/main/java/com/codesquad/todolist/TodolistApplication.java
@@ -2,7 +2,6 @@ package com.codesquad.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class TodolistApplication {
