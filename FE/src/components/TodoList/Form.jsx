69f0059bcb5205caa67c63a962d827a20b67113a
SEONKYU\difke <somedaycode@gmail.com>
2021-04-12 17:26:09 +0900

    [Add]: form 카드가 텍스트 길이에 따라 사이즈가 바뀐다

--- FE/src/components/TodoList/Form.jsx
+++ FE/src/components/TodoList/Form.jsx
@@ -1,46 +1,57 @@
+import { useRef } from 'react';
+import StyledForm from '../atoms/StyledForm';
+import { InputTitle, InputContent } from '../atoms/StyledInputs';
+import FormButtonsWrap from '../molecules/FormButtonsWrap';
 
-import {useRef} from 'react'
+const Form = ({ addCard, column, handleClickCancel }) => {
+  const titleInput = useRef();
+  const contentInput = useRef();
+  const nextID = useRef(5); // 임시 아이디
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newCard = {
+      id: nextID.current,
+      title: titleInput.current.value,
+      content: contentInput.current.value,
+      author: column.cards.author,
+    };
+    const cardAdded = [...column.cards, newCard];
+    column.cards = cardAdded;
+    nextID.current++;
+    addCard(column);
+  };
 
-const Form = ({addCard, column, handleClickCancel}) => {
-    const titleInput = useRef();
-    const contentInput = useRef();
-    const nextID = useRef(5); // 임시 아이디
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const newCard = {
-            id: nextID.current,
-            title: titleInput.current.value,
-            content: contentInput.current.value,
-            author: column.cards.author
-        }
-        const cardAdded = [...column.cards, newCard];
-        column.cards = cardAdded;
-        nextID.current++;
-        addCard(column)
+  const resize = () => {
+    const currentHeight = contentInput.current.clientHeight;
+    const scrollHeight = contentInput.current.scrollHeight;
+    if (currentHeight < scrollHeight) {
+      contentInput.current.style.height = `${
+        contentInput.current.scrollHeight + 2
+      }px`;
     }
+  };
 
-    const NewForm = () => (
-    <form onSubmit={handleSubmit}>
-    <input 
-        name="title" 
-        type="text" 
-        placeholder="제목을 적어주세요" 
-        ref={titleInput} 
-        autoComplete="off"></input>
-    <input 
-        name="content" 
-        type="text" 
-        placeholder="내용을 적어주세요" 
-        ref={contentInput} 
-        autoComplete="off"></input>
-    <div>
-    <button>등록</button>
-    <button onClick={handleClickCancel}>취소</button>
-    </div> 
-    </form>);
-    return (
-        <NewForm/>
-    )
-}
+  const NewForm = () => (
+    <StyledForm onSubmit={handleSubmit}>
+      <InputTitle
+        name="title"
+        type="text"
+        placeholder="제목을 적어주세요"
+        ref={titleInput}
+        autoComplete="off"
+      />
+      <InputContent
+        name="content"
+        type="text"
+        placeholder="내용을 적어주세요"
+        ref={contentInput}
+        autoComplete="off"
+        onKeyUp={resize}
+      />
+      <FormButtonsWrap handleClickCancel={handleClickCancel} />
+    </StyledForm>
+  );
+  return <NewForm />;
+};
 
-export default Form
+export default Form;
