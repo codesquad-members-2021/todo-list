27573512f2b7ac557407ab47340aae63fcc5f74e
woody <woojihye2339@gmail.com>
2021-04-12 22:58:39 +0900

    refactor: Remove unused methods in controllers (#16)
    - ApiCardController::list 삭제
    - ApiCardController::getCard 삭제
    - ApiColumnController::list 삭제

--- backend/src/main/java/com/team08/todolist/controller/ApiCardController.java
+++ backend/src/main/java/com/team08/todolist/controller/ApiCardController.java
@@ -1,12 +1,7 @@
 package com.team08.todolist.controller;
 
-import com.team08.todolist.DateTimeUtils;
 import com.team08.todolist.dto.CardDto;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/cards")
@@ -15,25 +10,6 @@ public class ApiCardController {
     @PostMapping
     public CardDto create(CardDto cardToCreate) {
         return cardToCreate;
-    }
-
-    @GetMapping
-    public List<CardDto> list() {
-        List<CardDto> cards = new ArrayList<>();
-        cards.add(new CardDto(1L, "title1",
-                "content1", true, DateTimeUtils.formatByPattern(LocalDateTime.now()), 65535));
-        cards.add(new CardDto(2L, "title2",
-                "content2", true, DateTimeUtils.formatByPattern(LocalDateTime.now()), 65535 * 2));
-        cards.add(new CardDto(3L, "title3",
-                "content3", false, DateTimeUtils.formatByPattern(LocalDateTime.now()), 65535 * 4));
-
-        return cards;
-    }
-
-    @GetMapping("/{cardId}")
-    public CardDto getCard(@PathVariable Long cardId) {
-        return new CardDto(cardId, "title1",
-                "content1", true, DateTimeUtils.formatByPattern(LocalDateTime.now()), 65535);
     }
 
     @PutMapping("/{cardId}")
--- backend/src/main/java/com/team08/todolist/controller/ApiColumnController.java
+++ backend/src/main/java/com/team08/todolist/controller/ApiColumnController.java
@@ -2,7 +2,6 @@ package com.team08.todolist.controller;
 
 import com.team08.todolist.DateTimeUtils;
 import com.team08.todolist.dto.CardDto;
-import com.team08.todolist.model.Column;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,16 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/columns")
 public class ApiColumnController {
-
-    @GetMapping
-    public List<Column> list() {
-        List<Column> columns = new ArrayList<>();
-        columns.add(new Column(1L, "ToDo", 65535, 1));
-        columns.add(new Column(2L, "Doing", 65535 * 2, 1));
-        columns.add(new Column(3L, "Done", 65535 * 4, 1));
-        return columns;
-    }
-
+    
     @GetMapping("/{columnId}")
     public List<CardDto> column(@PathVariable Long columnId) {
         List<CardDto> cards = new ArrayList<>();
