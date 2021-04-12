102233a6c379f3e5649fe630c38a39993649e2e5
95degree <95degree@naver.com>
2021-04-12 14:44:31 +0900

    feat : Card Entity에 필요한 정보 입력
    -To do List의 Card에 필요한 정보들의 생성자 및 getter 추가

--- backend/todolist/src/main/java/team10/Card.java
+++ backend/todolist/src/main/java/team10/Card.java
@@ -1,4 +1,66 @@
 package team10;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+
+import java.time.LocalDateTime;
+
 public class Card {
+
+    @Id
+    private Long id;
+    private String title;
+    private String contents;
+    private String category;
+
+    @Column(value = "date_time")
+    private LocalDateTime dateTime;
+    @Column(value = "is_deleted")
+    private boolean isDeleted = false;
+
+    protected Card() {
+    }
+
+    public Card(String title, String contents, String category) {
+        this.title = title;
+        this.contents = contents;
+        this.category = category;
+        this.dateTime = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public boolean getIsDeleted() {
+        return isDeleted;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", contents='" + contents + '\'' +
+                ", category='" + category + '\'' +
+                ", dateTime='" + LocalDateTime.now().toString() + '\'' +
+                ", isDeleted=" + isDeleted +
+                '}';
+    }
 }
