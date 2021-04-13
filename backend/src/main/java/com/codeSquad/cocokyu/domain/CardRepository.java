f3b3fe657403dfe901a7cf6ebd1794e9eef986ec
gisungchoi <chlrltjd159@naver.com>
2021-04-14 01:36:57 +0900

    refactor : priority 삭제
    - ios와 이야기 안끝남 그래서 삭제

--- backend/src/main/java/com/codeSquad/cocokyu/domain/CardRepository.java
+++ backend/src/main/java/com/codeSquad/cocokyu/domain/CardRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface CardRepository extends CrudRepository<Card, Long> {
-    @Query("select * from card c where not c.status = 'DELETED' ORDER BY c.priority")
+    @Query("select * from card c where not c.status = 'DELETED'")
     Iterable<Card> findByCardToNotDeleted();
 
     @Query("select * from log ORDER BY create_date_time DESC")
