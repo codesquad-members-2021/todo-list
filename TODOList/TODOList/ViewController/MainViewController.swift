1c3cc329b4b84d25beafc68a9d3cf47c72143447
okstring <milkoi7801@gmail.com>
2021-04-13 17:54:06 +0900

    Simple: set menu 간소화

--- TODOList/TODOList/ViewController/MainViewController.swift
+++ TODOList/TODOList/ViewController/MainViewController.swift
@@ -15,50 +15,15 @@ class MainViewController: UIViewController {
     }
     var menuView: UIView!
     var closeButton: UIButton!
+    var menuTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setMenuView()
+        self.makeMenuView()
+        self.makeTableView()
+        self.setConstraintOfTableView()
+        self.setCloseButton()
     }
-    
-    //MARK:TestAndTest
-    func setMenuView() {
-        self.menuView = UIView()
-        self.menuView.backgroundColor = .white
-        self.menuView.frame.size.width = self.view.frame.width * 0.35
-        self.menuView.frame.size.height = self.view.frame.height
-        self.menuView.frame.origin.x = self.view.frame.maxX
-        
-        var tableView = UITableView()
-        tableView = UITableView(frame: CGRect(x: 40, y: 60, width: self.view.frame.width * 0.3 - 60, height: self.view.frame.height))
-        tableView.separatorStyle = .none
-        tableView.allowsSelection = false
-        
-        tableView.dataSource = self
-        tableView.delegate = self
-        
-        let nibName = UINib(nibName: "HistoryCell", bundle: nil)
-        tableView.register(nibName, forCellReuseIdentifier: "historyCell")
-        
-        self.menuView.addSubview(tableView)
-        
-        tableView.reloadData()
-        
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.topAnchor.constraint(equalTo: self.menuView.topAnchor, constant: 60).isActive = true
-        tableView.bottomAnchor.constraint(equalTo: self.menuView.bottomAnchor, constant: 0).isActive = true
-        tableView.leadingAnchor.constraint(equalTo: self.menuView.leadingAnchor, constant: 40).isActive = true
-        tableView.trailingAnchor.constraint(equalTo: self.menuView.trailingAnchor, constant: 40).isActive = true
-        
-        guard let largeBold = UIImage(systemName: "xmark") else { return }
-        self.closeButton = UIButton(frame: CGRect(x: self.menuView.frame.width - 50.75, y: 30.75, width: 40, height: 40))
-        self.closeButton.tintColor = .black
-        self.closeButton.setImage(largeBold, for: .normal)
-        self.closeButton.addTarget(self, action: #selector(closeMenuView), for: .touchUpInside)
-        self.view.addSubview(self.menuView)
-        self.menuView.addSubview(self.closeButton)
-    }
-    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == Segues.willTODO {
@@ -83,6 +48,46 @@ class MainViewController: UIViewController {
         UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut) {
             self.menuView.frame.origin.x = self.view.frame.maxX
         }
+    }
+    
+    private func makeMenuView() {
+        self.menuView = UIView()
+        self.menuView.backgroundColor = .white
+        self.menuView.frame.size.width = self.view.frame.width * 0.35
+        self.menuView.frame.size.height = self.view.frame.height
+        self.menuView.frame.origin.x = self.view.frame.maxX
+        self.view.addSubview(self.menuView)
+    }
+    
+    private func makeTableView() {
+        self.menuTableView = UITableView()
+        self.menuTableView = UITableView(frame: CGRect(x: 40, y: 60, width: self.view.frame.width * 0.3 - 60, height: self.view.frame.height))
+        self.menuTableView.separatorStyle = .none
+        self.menuTableView.allowsSelection = false
+        self.menuTableView.dataSource = self
+        self.menuTableView.delegate = self
+        
+        let nibName = UINib(nibName: "HistoryCell", bundle: nil)
+        self.menuTableView.register(nibName, forCellReuseIdentifier: "historyCell")
+        self.menuView.addSubview(self.menuTableView)
+    }
+    
+    private func setConstraintOfTableView() {
+        self.menuTableView.translatesAutoresizingMaskIntoConstraints = false
+        self.menuTableView.topAnchor.constraint(equalTo: self.menuView.topAnchor, constant: 60).isActive = true
+        self.menuTableView.bottomAnchor.constraint(equalTo: self.menuView.bottomAnchor, constant: 0).isActive = true
+        self.menuTableView.leadingAnchor.constraint(equalTo: self.menuView.leadingAnchor, constant: 40).isActive = true
+        self.menuTableView.trailingAnchor.constraint(equalTo: self.menuView.trailingAnchor, constant: 40).isActive = true
+    }
+    
+    private func setCloseButton() {
+        guard let largeBold = UIImage(systemName: "xmark") else { return }
+        self.closeButton = UIButton(frame: CGRect(x: self.menuView.frame.width - 50.75, y: 30.75, width: 40, height: 40))
+        self.closeButton.tintColor = .black
+        self.closeButton.setImage(largeBold, for: .normal)
+        self.closeButton.addTarget(self, action: #selector(closeMenuView), for: .touchUpInside)
+        
+        self.menuView.addSubview(self.closeButton)
     }
 }
 
