6783014e83e73f098cfaed61545ee706f9a06844
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-04-13 14:57:11 +0900

    [Style]button 위치 변경

--- src/Component/Header/Navigator.jsx
+++ src/Component/Header/Navigator.jsx
@@ -6,18 +6,23 @@ import styled from 'styled-components'
 const NavigatorBlock = styled.div`
   top: 0;
   right: 0;
-  transform: ${({ toggle }) => (toggle === 0 ? 'translateX(+20rem)' : 'translateX(0)')};
+  transform: ${({ toggle }) =>
+    toggle === 0 ? 'translateX(+20rem)' : 'translateX(0)'};
   width: 25%;
   height: 100%;
   background: white;
   border: 1px solid black;
   display: flex;
-  align-items: flex-start;
+  align-items: flex-end;
   flex-direction: column;
   position: absolute;
   box-sizing: border-box;
   padding: 2rem;
   transition: 0.2s ease-out;
+  button {
+    margin-right: 2.9rem;
+    margin-top: 0.9rem;
+  }
 `
 
 export default function Navigator ({ toggle, onClick }) {
