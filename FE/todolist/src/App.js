07efc78e7bff9303354842719611a43cd6df54db
Hannah Park <ha3158987@gmail.com>
2021-04-14 02:07:50 +0900

    refactor: :recycle: App 컴포넌트 리팩토링 - open 상태추가 - noteList, setNoteList App으로 이전

--- FE/todolist/src/App.js
+++ FE/todolist/src/App.js
@@ -1,22 +1,25 @@
 import './reset.css';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import HeaderTitle from './header/HeaderTitle';
-import ColumnList from './content/column/ColumnList';
+// import ColumnList from './content/column/ColumnList';
 import Sidebar from './sidebar/Sidebar';
-
+//공유하는 속성이 있다면 app(공유하는 최상단)에다가 useContext/ store
 function App() {
   const sidebar = useRef();
+  const [ open, setOpen ] = useState(false);
+  const [ noteList, setNoteList ] = useState([])
 
   const showSidebar = () => {
     const sidebarPage = sidebar.current;
     sidebarPage.style.transform = 'translateX(0px)';
+    setOpen(open => !open);
   }
 
   return (
     <>
       <HeaderTitle handleClick={showSidebar}/>
-      <ColumnList />
-      <Sidebar sidebarRef={sidebar}/>
+      {/* <ColumnList setNoteList={setNoteList}/> */}
+      <Sidebar sidebarRef={sidebar} open={open} noteList={noteList} setNoteList={setNoteList}/>
     </>
   );
 }
