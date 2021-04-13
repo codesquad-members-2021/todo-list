c691728751f39544d67f6033886710d5194dd5ea
정이삭 <isaac56@naver.com>
2021-04-13 16:01:36 +0900

    [#26] chore: Modify importing intial data sql
    초기 값 삽입하는 sql문 수정

--- BE/todo/src/main/resources/data.sql
+++ BE/todo/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO `user` (`user_id`,`password`) VALUES ('noel', '1234');
-INSERT INTO `card` (`user`, `title`, `content`, `priority`, `column_type`) VALUES (1, '제목입니다.', '내용입니다.', 1, '할일');
-INSERT INTO `card` (`USER`, `TITLE`, `CONTENT`, `PRIORITY`, `COLUMN_TYPE`) VALUES (1, '테스트1', '내용', 1.3, '하는중');
-INSERT INTO `card` (`USER`, `TITLE`, `CONTENT`, `PRIORITY`, `COLUMN_TYPE`) VALUES (1, '테스트2', '내용', 1.6, '완료');
-INSERT INTO `history` (`CARD`, `ACTION`, `DATE`, `FROM`, `TO`) VALUES ('1', '이동', now(), '할일', '하는중');
-INSERT INTO `history` (`CARD`, `ACTION`, `DATE`, `FROM`, `TO`) VALUES ('2', '추가', now(), null, '하는중');
-INSERT INTO `history` (`CARD`, `ACTION`, `DATE`, `FROM`, `TO`) VALUES ('2', '삭제', now(), '할일', null);
+INSERT INTO `card` (`user`, `title`, `content`, `priority`, `column_type`) VALUES (1, '제목입니다.', '내용입니다.', 1, 'TODO');
+INSERT INTO `card` (`USER`, `TITLE`, `CONTENT`, `PRIORITY`, `COLUMN_TYPE`) VALUES (1, '테스트1', '내용', 1.3, 'DOING');
+INSERT INTO `card` (`USER`, `TITLE`, `CONTENT`, `PRIORITY`, `COLUMN_TYPE`) VALUES (1, '테스트2', '내용', 1.6, 'DONE');
+INSERT INTO `history` (`CARD`, `ACTION`, `DATE`, `FROM`, `TO`) VALUES ('1', '이동', now(), 'TODO', 'DOING');
+INSERT INTO `history` (`CARD`, `ACTION`, `DATE`, `FROM`, `TO`) VALUES ('2', '추가', now(), null, 'DOING');
+INSERT INTO `history` (`CARD`, `ACTION`, `DATE`, `FROM`, `TO`) VALUES ('2', '삭제', now(), 'DONE', null);
