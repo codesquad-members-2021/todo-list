1e6104b155b0c07002628839594d11e9e4cfa8ec
kowoohyuk <kowoohyuk91@gmail.com>
2021-04-13 18:12:17 +0900

    [Style] GlobalStyle
    - body, button, input 기본 스타일 설정
    - font-family: "DungGeunMo"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import TodoContainer from "./Component/Todo/TodoContainer";
+import { createGlobalStyle } from "styled-components";
 import Header from "./Component/Header/Header";
+import TodoContainer from "./Component/Todo/TodoContainer";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #f2f2f2;
+  }
+  button {
+    background-color: transparent;
+    border:none;
+    cursor: pointer;
+  }
+  input {
+    border: none;
+  }
+  input:focus {
+    outline:none;
+  }
+  button:focus {
+    outline:none;
+  }
+  * {
+    box-sizing: border-box;
+    font-family: "DungGeunMo";
+  }
+`;
 
 function App() {
   return (
     <>
-      <Header></Header>
+      <GlobalStyle />
+      <Header />
       <TodoContainer />
     </>
   );
