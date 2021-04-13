5cd69c700bdda4b32dee14c722f388c10a014051
Lia <judykim257@gmail.com>
2021-04-13 17:27:23 +0900

    Code : contextMenu 팝업 기능만 구현
    세부 기능 구현 X

--- Todo_List/Todo_List/Controller/TodoDelegate.swift
+++ Todo_List/Todo_List/Controller/TodoDelegate.swift
@@ -13,9 +13,20 @@ class TodoDelegate: NSObject, UITableViewDelegate {
         return UITableView.automaticDimension
     }
     
-//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//            if (editingStyle == .delete){
-//            }
-//        }
+    func tableView(_ tableView: UITableView, contextMenuConfigurationForRowAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+        return UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { (menu) -> UIMenu? in
+            let moveAction = UIAction(title: NSLocalizedString("완료한 일로 이동", comment: ""), image: nil) { (action) in
+                
+            }
+            let modifyAction = UIAction(title: NSLocalizedString("수정하기", comment: ""), image: nil) { (action) in
+                
+            }
+            let deleteAction = UIAction(title: NSLocalizedString("삭제하기", comment: ""), image: UIImage(systemName: "trash"), attributes: .destructive) { (action) in
+                
+            }
+            
+            return UIMenu(title: "", children: [moveAction, modifyAction, deleteAction])
+        }
+    }
     
 }
