4ef07f29089b7b41723aa8db0ceeceb423870899
kowoohyuk <kowoohyuk91@gmail.com>
2021-04-13 14:54:06 +0900

    [Feat] CreateTodo
    - 렌더링
    - 스타일 컴포넌트 적용

--- src/Component/Todo/CreateTodo.jsx
+++ src/Component/Todo/CreateTodo.jsx
@@ -3,21 +3,41 @@ import styled from "styled-components";
 import Button from "../Button";
 
 const CreateTodoBlock = styled.div`
+  background-color: #fff;
+  border: 1px solid #ffcc00;
+  border-radius: 0.3rem;
+  padding: 0.8rem;
 `;
 
 const InputTitle = styled.input.attrs({
   type: "text"
 })`
-
+  width: 100%;
+  font-size: 1rem;
+  font-weight: 600;
 `;
 
 const InputContent = styled.input.attrs({
   type: "text"
 })`
-
+  width: 100%;
+  margin-bottom: 0.7rem;
 `;
 
 const Buttons = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 0.4rem;
+  button {
+    border-radius: 0.2rem;
+    background-color: #bbb;
+    padding: 0.4rem 1rem;
+    opacity: 0.6;
+    &:last-child {
+      color: #fff;
+      background-color: #ffcc00;
+    }
+  }
 `;
 
 
