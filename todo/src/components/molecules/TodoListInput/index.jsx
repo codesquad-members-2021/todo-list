153ebcbe8ea8f32da0a070f0f1fd0cde5ca83663
jeonyeonkyu <ehb6915@naver.com>
2021-04-13 14:26:56 +0900

    Fix: Div 제거 fix #11

--- todo/src/components/molecules/TodoListInput/index.jsx
+++ todo/src/components/molecules/TodoListInput/index.jsx
@@ -1,31 +1,38 @@
 import React from "react";
-import styled from "styled-components";
 import TitleInput from "../../atoms/TitleInput";
 import ContentInput from "../../atoms/ContentInput";
 import SmallButton from "../../atoms/Buttons/SmallButton";
 import Image from "../../atoms/Image";
 import closeButton from "../../../images/closeButton.svg";
 import ButtonList from "../../atoms/Buttons/ButtonList";
+import styled from 'styled-components';
+import { useState } from 'react';
 
 const Div = styled.div`
-  position: relative;
-  width: 308px;
-  padding: 16px;
-  margin: 16px;
-  background: #fff;
-  border-radius: 5px;
-`;
+  text-align: center;
+  margin: 15px 0 0 0;
+`
 
-const TodoListInput = () => {
+const TodoListInput = ({ title, content, toggleActions }) => {
+
+  const [titleValue, setTitle] = useState(title);
+  const [contentValue, setContent] = useState(content);
+
+  const onChange = (setState, { target }) => {
+    setState(target.value);
+  }
+
   return (
-    <Div>
+    <>
       <SmallButton _position="absolute" _right="3px">
         <Image src={closeButton} _width="10px" />
       </SmallButton>
-      <TitleInput placeholder="제목을 적어주세요"></TitleInput>
-      <ContentInput placeholder="내용을 적어주세요"></ContentInput>
-      <ButtonList></ButtonList>
-    </Div>
+      <TitleInput placeholder="제목을 적어주세요" onChange={(e) => onChange(setTitle, e)} value={titleValue} />
+      <ContentInput placeholder="내용을 적어주세요" onChange={(e) => onChange(setContent, e)} value={contentValue} />
+      <Div>
+        <ButtonList toggleActions={toggleActions} />
+      </Div>
+    </>
   );
 };
 
