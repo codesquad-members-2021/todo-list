ce961bc5dddc144af597ccd3bb66ba12a825b1d6
정이삭 <isaac56@naver.com>
2021-04-13 17:53:52 +0900

    [#26] Handle NotFoundException in GlobalAiExceptionHandler
    GlobalApiExceptionHandler에서 NotFoundException을 처리할 수 있도록 했습니다.

--- BE/todo/src/main/java/team9/todo/controller/GlobalApiExceptionHandler.java
+++ BE/todo/src/main/java/team9/todo/controller/GlobalApiExceptionHandler.java
@@ -4,11 +4,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import team9.todo.domain.ApiResult;
 import team9.todo.exception.NotAuthorizedException;
+import team9.todo.exception.NotFoundException;
 
 @RestControllerAdvice
 public class GlobalApiExceptionHandler {
-    @ExceptionHandler(NotAuthorizedException.class)
-    public ApiResult notAuthorizedException(NotAuthorizedException exception) {
+    @ExceptionHandler({
+            NotAuthorizedException.class,
+            NotFoundException.class
+    })
+    public ApiResult notAuthorizedException(RuntimeException exception) {
         return ApiResult.failed(exception);
     }
 }
