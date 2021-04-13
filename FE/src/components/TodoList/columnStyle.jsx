34941c4e3e3eef568b072b9c445e23f7b011e00c
SEONKYU\difke <somedaycode@gmail.com>
2021-04-13 16:37:36 +0900

    [Delete]: 파일 구조 변경으로 불필요한 파일 삭제

--- FE/src/components/TodoList/columnStyle.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const DivFlex = styled.div`
-  display: flex;
-`;
-
-const ColumnContainer = ({ children }) => <DivFlex>{children}</DivFlex>;
-
-export { ColumnContainer };
