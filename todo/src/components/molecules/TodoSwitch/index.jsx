fe4636f4336420637199684b8fc1b6bdbbe6bf2a
jeonyeonkyu <ehb6915@naver.com>
2021-04-13 14:28:49 +0900

    Feat: TodoInput TodoList를 스위칭해주는 TodoSwitch 구현 fix #11

---
+++ todo/src/components/molecules/TodoSwitch/index.jsx
@@ -0,0 +1,47 @@
+import TodoListInput from '../TodoListInput';
+import TodoListItem from '../TodoListItem';
+import styled, { css } from "styled-components";
+import useToggle from '../../../hooks/useToggle';
+
+const Div = styled.div`
+  position: relative;
+  width: 308px;
+  padding: 16px;
+  margin: 16px;
+  background: #fff;
+  border: 2px solid #000;
+  border-radius: 5px;
+  cursor: pointer;
+  ${({ hover }) => hover && css`
+        &:hover{
+        border:2px solid blue;
+        background: #e0efff;
+      }
+  `}
+`;
+
+const TodoSwitch = ({ title, content, author }) => {
+  const [isInput, toggleActions] = useToggle(false);
+
+  let template;
+
+  if (isInput) {
+    template = (
+      <Div>
+        <TodoListInput  {...{ title, content, toggleActions }} />
+      </Div>);
+  } else {
+    template = (
+      <Div hover onClick={toggleActions.toggle}>
+        <TodoListItem {...{ title, content, author }} />
+      </Div>);
+  }
+
+  return (
+    <>
+      {template}
+    </>
+  )
+}
+
+export default TodoSwitch;
