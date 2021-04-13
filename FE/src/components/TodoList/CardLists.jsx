e241ab680db3a0f613cb4f747cdfa7f3332649a2
SEONKYU\difke <somedaycode@gmail.com>
2021-04-14 04:05:50 +0900

    [Add]: cardList부분에서 Card를 떼어냈다.
    cardList안에서 Card 컴포넌트를 따로 만든 후 카드 수정과 취소가 가능하도록 변경하였다

--- FE/src/components/TodoList/CardLists.jsx
+++ FE/src/components/TodoList/CardLists.jsx
@@ -1,49 +1,17 @@
-import { useState } from 'react';
-import styled from 'styled-components';
-import { InputTitle, InputContent } from '../atoms/StyledInputs';
+import Card from './Card';
 
-const CardLists = ({ cards }) => {
-  const [inputs, setInputs] = useState({
-    title: '',
-    content: '',
-  });
-
-  const handleClickCard = () => {
-    console.log('clicked');
-  };
-  const cardlists = cards.map((cards) => {
-    const { id, title, content, author } = cards;
-    return (
-      <CardContainer key={id + title}>
-        <li>
-          <div onDoubleClick={handleClickCard}>
-            <CardTitleInput value={title} readOnly={true} />
-            <CardContentTextArea value={content} readOnly={true} />
-          </div>
-          <span>{author}</span>
-        </li>
-      </CardContainer>
-    );
-  });
+const CardLists = ({ cards, inputs, onChange, checkInputValue, hasInput }) => {
+  const cardlists = cards.map((cards) => (
+    <Card
+      key={cards.id}
+      cards={cards}
+      onChange={onChange}
+      inputs={inputs}
+      checkInputValue={checkInputValue}
+      hasInput={hasInput}
+    />
+  ));
 
   return <>{cardlists}</>;
 };
 export default CardLists;
-
-const CardContainer = styled.div`
-  margin-bottom: 1rem;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin-bottom: 1rem;
-  padding: 16px;
-  width: 308px;
-  height: auto;
-  background: #ffffff;
-  box-sizing: border-box;
-  box-shadow: 0px 1px 30px rgba(224, 224, 224, 0.3);
-  border-radius: 6px;
-  border: 0;
-`;
-const CardTitleInput = styled(InputTitle)``;
-const CardContentTextArea = styled(InputContent)``;
