3b51f7f5aa0a8c56a8dd5dc989ce72e87e3f1438
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-04-13 15:17:59 +0900

    [Refoctor]setToggle refoctor

--- src/Component/Header/HeadeTitle.jsx
+++ src/Component/Header/HeadeTitle.jsx
@@ -4,35 +4,24 @@ import Navigator from './Navigator'
 import { FaChevronLeft } from 'react-icons/fa'
 import styled from 'styled-components'
 
-
 const HeadTitleBlock = styled.div`
-width: 100%;
-height: 7rem;
-background: #f5f5f7;
-display: flex;
-align-items: center;
-justify-content: space-between;
-font-family: Noto Sans KR;
-font-size: 32px;
-line-height: 46px;
-padding: 0 5rem;
-box-sizing: border-box;
+  width: 100%;
+  height: 7rem;
+  background: #f5f5f7;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-family: Noto Sans KR;
+  font-size: 32px;
+  line-height: 46px;
+  padding: 0 5rem;
+  box-sizing: border-box;
 `
 
-
 export default function HeadTitle ({ children }) {
-  const [toggle, setToggle] = useState(0)
+  const [toggle, setToggle] = useState(false);
 
-  const onClick = () => {
-    setToggle(toggle => {
-      if (toggle) {
-        return toggle - 1
-      } else {
-        return toggle + 1
-      }
-    });
-  }
-
+  const onClick = () => setToggle(!toggle);
 
   return (
     <HeadTitleBlock>
@@ -40,7 +29,7 @@ export default function HeadTitle ({ children }) {
       <Button onClick={onClick}>
         <FaChevronLeft />
       </Button>
-      <Navigator toggle={toggle} onClick={onClick}/>
+      <Navigator toggle={toggle} onClick={onClick} />
     </HeadTitleBlock>
   )
 }
--- src/Component/Header/Navigator.jsx
+++ src/Component/Header/Navigator.jsx
@@ -7,7 +7,7 @@ const NavigatorBlock = styled.div`
   top: 0;
   right: 0;
   transform: ${({ toggle }) =>
-    toggle === 0 ? 'translateX(+20rem)' : 'translateX(0)'};
+    toggle ? 'translateX(+20rem)' : 'translateX(0)'};
   width: 25%;
   height: 100%;
   background: white;
