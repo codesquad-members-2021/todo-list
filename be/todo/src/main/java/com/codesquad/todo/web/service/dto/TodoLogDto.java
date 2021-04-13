b6e62ee97c4cf1a7e98c6fb542b17164b13bb99b
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-04-13 23:18:02 +0900

    feat : TodoLog 목록조회 API의 버그 수정
    - from, to ColumnTitle과 taskTitle이 누락된 문제를 해결함

--- be/todo/src/main/java/com/codesquad/todo/web/service/dto/TodoLogDto.java
+++ be/todo/src/main/java/com/codesquad/todo/web/service/dto/TodoLogDto.java
@@ -7,12 +7,18 @@ import java.time.LocalDateTime;
 public class TodoLogDto {
     private final long id;
     private final String action;
+    private final String fromColumnTitle;
+    private final String toColumnTitle;
+    private final String taskTitle;
     private final String authorName;
     private final LocalDateTime createdDateTime;
 
     public TodoLogDto(TodoLog todoLog, String authorName) {
         this.id = todoLog.getId();
         this.action = todoLog.getAction();
+        this.fromColumnTitle = todoLog.getFromColumnTitle();
+        this.toColumnTitle = todoLog.getToColumnTitle();
+        this.taskTitle = todoLog.getTaskTitle();
         this.authorName = authorName;
         this.createdDateTime = todoLog.getCreatedDateTime();
     }
@@ -31,5 +37,17 @@ public class TodoLogDto {
 
     public LocalDateTime getCreatedDateTime() {
         return createdDateTime;
+    }
+
+    public String getFromColumnTitle() {
+        return fromColumnTitle;
+    }
+
+    public String getToColumnTitle() {
+        return toColumnTitle;
+    }
+
+    public String getTaskTitle() {
+        return taskTitle;
     }
 }
