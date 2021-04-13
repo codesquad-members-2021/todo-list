fe41c659f41335b3857c387a853f400e1f5e66a4
jeonyeonkyu <ehb6915@naver.com>
2021-04-13 14:26:56 +0900

    Fix: Div 제거 fix #11

--- todo/src/components/molecules/TodoListItem/index.jsx
+++ todo/src/components/molecules/TodoListItem/index.jsx
@@ -3,33 +3,17 @@ import SmallButton from "../../atoms/Buttons/SmallButton";
 import Image from "../../atoms/Image";
 import Span from "../../atoms/Span";
 import closeButton from "../../../images/closeButton.svg";
-import styled from "styled-components";
-
-const Div = styled.div`
-  position: relative;
-  width: 308px;
-  padding: 16px;
-  margin: 16px;
-  background: #fff;
-  border: 2px solid #000;
-  border-radius: 5px;
-  cursor: pointer;
-  &:hover{
-    border:2px solid blue;
-    background: #e0efff;
-  }
-`;
 
 const TodoListItem = ({ title, content, author }) => {
   return (
-    <Div>
+    <>
       <SmallButton _position="absolute" _right="3px">
         <Image src={closeButton} _width="10px" />
       </SmallButton>
       <Span _display="block" _color="#000" _fontSize="17px" _fontWeight="700">{title}</Span>
       <Span _display="block" _color="#000" _fontSize="16px">{content}</Span>
       <Span _display="block" _color="#969595" _fontSize="13px">{author}</Span>
-    </Div>
+    </>
   );
 };
 
