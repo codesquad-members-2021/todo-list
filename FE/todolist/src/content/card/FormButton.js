76c25833d7aff0fa123758c238b987e0f7288bef
eve712 <62237639+eve712@users.noreply.github.com>
2021-04-15 05:44:33 +0900

    chore: 파일 이름 변경, 버튼 색 변경

---
+++ FE/todolist/src/content/card/FormButton.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+const BtnContainer = styled.div`
+    width: 276px;
+    height: 40px;
+    display: flex;
+    justify-content: space-between;
+    button {
+        width: 134px;
+        height: 40px;
+        border-radius: 6px;
+        outline: none;
+        border: none;
+        background: #E0E0E0;
+        color: #828282;
+        cursor: pointer;
+    }
+    button:last-child {
+        background: #86C6FF;
+        color:  rgba(255, 255, 255, 0.4);
+    }
+`
+
+const FormButton = ({resetInputState, setInputState}) => {
+    return(
+            <BtnContainer>
+                <button onClick={resetInputState}>취소</button>
+                <button onClick={setInputState}>저장</button>
+            </BtnContainer>
+        )      
+}
+
+export default FormButton;
