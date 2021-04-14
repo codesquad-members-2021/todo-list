0dc7f09ff1985ad9dc5ef885a9ca85f4db093408
dong <ultraraptor@naver.com>
2021-04-14 14:43:50 +0900

    refactor : BoardRepository 기능 추가 및 수정
    - Iterable 로 반환되던 메서드를 List로 반환하도록 수정
    - findAll 메서드  추가

--- backend/todolist/src/main/java/team10/todolist/repository/BoardRepository.java
+++ backend/todolist/src/main/java/team10/todolist/repository/BoardRepository.java
@@ -3,6 +3,9 @@ package team10.todolist.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import team10.todolist.domain.Board;
+
+import java.util.List;
+
 @Repository
 public interface BoardRepository extends CrudRepository<Board,Long> {
     Iterable<Board> findByCategory(String category);
