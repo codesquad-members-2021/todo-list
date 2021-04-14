6ad82418307cadaf75ff1f879e74e7dac122f0f4
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-04-14 22:36:26 +0900

    test : 안쓰는 import 제거

--- be/todo/src/test/java/com/codesquad/todo/web/domain/TodoUserRepositoryTest.java
+++ be/todo/src/test/java/com/codesquad/todo/web/domain/TodoUserRepositoryTest.java
@@ -10,8 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
