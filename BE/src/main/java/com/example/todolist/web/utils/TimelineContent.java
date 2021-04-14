e2f65fb36ed40c88376affed2a8887a49b32561e
Tree <gmldbs1109@naver.com>
2021-04-14 16:45:47 +0900

    Refactor : 상수 추가, StringBuilder 추가하여 코드 리팩토링

--- BE/src/main/java/com/example/todolist/web/utils/TimelineContent.java
+++ BE/src/main/java/com/example/todolist/web/utils/TimelineContent.java
@@ -1,42 +1,53 @@
 package com.example.todolist.web.utils;
 
 import com.example.todolist.domain.work.Work;
-import com.example.todolist.web.dto.RequestMoveWorkDto;
+import com.example.todolist.web.dto.MoveWorkRequestDTO;
 
 public class TimelineContent {
+
+    final static String SAVE = " 등록하였습니다.";
+    final static String UPDATE = " 수하였습니다.";
+    final static String DELETE = " 삭제하였습니다.";
+    final static String MOVE = " 이동하였습니다.";
+    final static String TODO = "해야할 일";
+    final static String DOING = "하고 있는 일";
+    final static String DONE = "완료한 일";
+
+
     public static String makeTimelineContent(Work work, String method) {
-        String content = "";
-        content += getStatus(work.getStatus()) + "에 ";
-        content += verifyJongsung(work.getTitle(), "을", "를");
+        StringBuilder contentBuilder = new StringBuilder();
+        contentBuilder.append(getStatus(work.getStatus()) + "에 ");
+        contentBuilder.append(verifyJongsung(work.getTitle(), "을", "를"));
+
 
         switch(method) {
             case "save" :
-                return content + " 등록하였습니다.";
+                contentBuilder.append(SAVE);
             case "update" :
-                return content + " 수정하였습니다.";
+                contentBuilder.append(UPDATE);
             case "delete" :
-                return content + " 삭제하였습니다.";
-            default :
-                return "";
+                contentBuilder.append(DELETE);
         }
+
+        return contentBuilder.toString();
     }
 
-    public static String moveContent(Work work, RequestMoveWorkDto workDto) {
-        String content = "";
-        content += verifyJongsung(work.getTitle(), "을", "를");
-        content += " " + getStatus(work.getStatus()) + "에서 ";
-        content += getStatus(workDto.toEntity().getStatus()) + "로 이동하였습니다.";
-        return content;
+    public static String moveContent(Work work, MoveWorkRequestDTO workDto) {
+        StringBuilder contentBuilder = new StringBuilder();
+        contentBuilder.append(verifyJongsung(work.getTitle(), "을", "를"));
+        contentBuilder.append(" " + getStatus(work.getStatus()) + "에서 ");
+        contentBuilder.append(getStatus(workDto.toEntity().getStatus()) + "로 " + MOVE);
+        return contentBuilder.toString();
     }
 
     public static String getStatus(int status) {
         switch(status) {
             case 1 :
-                return  "해야할 일";
+                return  TODO;
             case 2 :
-                return "하고 있는 일";
+                return DOING;
             case 3 :
-                return  "완료한 일";
+                return  DONE;
             default :
                 return "";
         }
