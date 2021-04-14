0c8f6546b22cf36d3b831824fca14d8e5b698daa
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-04-14 20:29:49 +1200

    LogRepository 생성
    - CardRepository에서 꺼내주던 Log를 분리

--- backend/src/main/java/com/codeSquad/cocokyu/domain/LogRepository.java
+++ backend/src/main/java/com/codeSquad/cocokyu/domain/LogRepository.java
@@ -1,14 +1,13 @@
 package com.codeSquad.cocokyu.domain;
 
-import com.codeSquad.cocokyu.domain.model.Card;
 import com.codeSquad.cocokyu.domain.model.Log;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface CardRepository extends CrudRepository<Card, Long> {
-    @Query("select * from card c where not c.status = 'DELETED'")
-    Iterable<Card> findByCardToNotDeleted();
-}
+public interface LogRepository extends CrudRepository<Log, Long> {
 
+    @Query("select * from log ORDER BY create_date_time DESC")
+    List<Log> findAllLog();
+}
