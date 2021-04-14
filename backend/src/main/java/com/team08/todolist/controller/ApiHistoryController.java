b31a0583069c95f55d21c58b37ed2d939e7561cf
woody <woojihye2339@gmail.com>
2021-04-14 14:41:10 +0900

    chore: Add various dummy data in histories (#16)
    - ApiHistoryController::list에 더미 데이터 추가
    - 다른 시간대의 히스토리 데이터를 7개 생성

--- backend/src/main/java/com/team08/todolist/controller/ApiHistoryController.java
+++ backend/src/main/java/com/team08/todolist/controller/ApiHistoryController.java
@@ -15,9 +15,16 @@ public class ApiHistoryController {
 
     @GetMapping
     public List<HistoryDto> list() {
+
+        LocalDateTime timeOffset = LocalDateTime.now();
         List<HistoryDto> histories = new ArrayList<>();
-        histories.add(new HistoryDto( "August", LocalDateTime.now(),"title1", "add", null, "ToDo"));
-        histories.add(new HistoryDto("roach", LocalDateTime.now(), "title2", "move", "ToDo", "Doing"));
+        histories.add(new HistoryDto("August", timeOffset.plusMinutes(3L), "title1", "add", null, "ToDo"));
+        histories.add(new HistoryDto("roach", timeOffset.plusMinutes(5L), "title2", "move", "ToDo", "Doing"));
+        histories.add(new HistoryDto("Woody", timeOffset.plusHours(4L), "title3", "update", "ToDo", null));
+        histories.add(new HistoryDto("Downey", timeOffset.plusDays(5L).plusSeconds(40), "title4", "remove", "Done", null));
+        histories.add(new HistoryDto("Downey", timeOffset.plusDays(1L).plusHours(5L), "title5", "add", "ToDo", null));
+        histories.add(new HistoryDto("ZG", timeOffset.plusWeeks(1L), "title6", "move", "Doing", "Done"));
+        histories.add(new HistoryDto("ZG", timeOffset.plusWeeks(1L).plusDays(3L), "title7", "move", "ToDo", "Doing"));
         return histories;
     }
 }
