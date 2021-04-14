5825e7079a1859f4f89b871a0246b9db032ba3b7
noel <68936@naver.com>
2021-04-14 14:55:49 +0900

    [#26] refactor: History 관련 Join결과를 HistoryRepository가 갖게 변경함.
    HistoryWithCardTitleRepsitory이 불필요하다고 느껴서 제거함.

--- BE/todo/src/main/java/team9/todo/repository/HistoryRepository.java
+++ BE/todo/src/main/java/team9/todo/repository/HistoryRepository.java
@@ -1,7 +1,15 @@
 package team9.todo.repository;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import team9.todo.domain.History;
+import team9.todo.domain.HistoryWithCardTitle;
+
+import java.util.List;
 
 public interface HistoryRepository extends CrudRepository<History, Long> {
+    @Query("SELECT `card`.`title` as `card_title`, `history`.`action` as `action`, `history`.`DATE` as `date`, `history`.`from` as `from`, `history`.`to` as `to` FROM `history` " +
+            "INNER JOIN `card` ON `history`.`CARD` = `card`.`ID` " +
+            "INNER JOIN `user` ON `card`.`USER` = `user`.`ID` AND `user`.`ID` = :userId")  //m :userId는 인자로 들어온 userId가 들어감
+    public List<HistoryWithCardTitle> findAllByUserId(Long userId);
 }
