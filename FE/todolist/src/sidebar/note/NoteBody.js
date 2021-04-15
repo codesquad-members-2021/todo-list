08e685df2567f0ca0faf9c7379cb442c560d1d0c
Hannah Park <ha3158987@gmail.com>
2021-04-16 00:44:24 +0900

    refactor: :recycle: switch문에서 객체형식으로 변경

--- FE/todolist/src/sidebar/note/NoteBody.js
+++ FE/todolist/src/sidebar/note/NoteBody.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 
 const NoteBodyStyle = styled.div`
@@ -9,27 +9,21 @@ const NoteBodyStyle = styled.div`
 const NoteBody = ({ noteData }) => {
 
     const switchMsgToKor = (msg) => {
-        switch(msg) {
-            case "TODO":
-                return "해야할 일";
-            case "PROGRESS":
-                return "하고 있는 일";
-            case "DONE":
-                return "완료한 일";
-            case "CREATE":
-                return "등록";
-            case "MOVE":
-                return "이동";
-            case "REMOVE":
-                return "삭제";
-            case "UPDATE":
-                return "변경";
+        const actionObj = {
+            "TODO":"해야할 일",
+            "PROGRESS":"하고 있는 일",
+            "DONE": "완료한 일",
+            "CREATE": "등록",
+            "MOVE": "이동",
+            "REMOVE": "삭제",
+            "UPDATE": "변경"
         }
+        return actionObj[msg];
     }
 
     const createMsg = ({ beforeStatus, afterStatus, title, action }) => {
         const updateAndMove = `${title}를(을) ${switchMsgToKor(beforeStatus)}에서 ${switchMsgToKor(afterStatus)}로 ${switchMsgToKor(action)}하였습니다.`;
-        const createAndRemove = `${title}를(을) ${switchMsgToKor(beforeStatus)}에 ${switchMsgToKor(action)}하였습니다.`
+        const createAndRemove = `${title}를(을) ${switchMsgToKor(beforeStatus)}에(서) ${switchMsgToKor(action)}하였습니다.`
         const message = beforeStatus === afterStatus ? createAndRemove : updateAndMove;
         return message;
     }
