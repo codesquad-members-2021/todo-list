d1f1769fe01676301d049fdf7e2f202b15d650e4
95degree <95degree@naver.com>
2021-04-15 15:24:56 +0900

    feat : 정해진 갯수의 log row들을 반환하는 controller 추가
    - 최신 시간 순으로 정렬해서 정해진 갯수만 보내준다.

--- backend/todolist/src/main/java/team10/todolist/controller/LogController.java
+++ backend/todolist/src/main/java/team10/todolist/controller/LogController.java
@@ -1,6 +1,7 @@
 package team10.todolist.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team10.todolist.domain.Log;
@@ -22,4 +23,10 @@ public class LogController {
     public List<Log> readAll() {
         return logService.readAll();
     }
+
+    @GetMapping("/lists/{limitNum}")
+    public List<Log> readUptoLimitNumber(@PathVariable int limitNum){
+        return logService.readUptoLimitNumber(limitNum);
+    }
+
 }
--- backend/todolist/src/main/java/team10/todolist/repository/LogRepository.java
+++ backend/todolist/src/main/java/team10/todolist/repository/LogRepository.java
@@ -1,6 +1,8 @@
 package team10.todolist.repository;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import team10.todolist.domain.Log;
 
@@ -8,5 +10,9 @@ import java.util.List;
 
 @Repository
 public interface LogRepository extends CrudRepository<Log, Long> {
+
     List<Log> findAll();
+
+    @Query("select * from log order by date_time desc limit :limitRow")
+    List<Log> findByDateTimeAndSort(@Param("limitRow") int limitRow);
 }
