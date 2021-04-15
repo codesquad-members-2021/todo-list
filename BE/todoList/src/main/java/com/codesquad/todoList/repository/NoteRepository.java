219f9b99466ab4b3ed4366cfef54855438c250f5
tmdgusya <tmdgusya@naver.com>
2021-04-15 23:52:54 +0900

    [#74] feat: :hamster: Note 최신순으로 정렬하기
    CREATE_DATETIME 기반으로, ORDER BY DESC 를 통해서 반환

--- BE/todoList/src/main/java/com/codesquad/todoList/repository/NoteRepository.java
+++ BE/todoList/src/main/java/com/codesquad/todoList/repository/NoteRepository.java
@@ -2,12 +2,13 @@ package com.codesquad.todoList.repository;
 
 import com.codesquad.todoList.entity.Action;
 import com.codesquad.todoList.entity.Note;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface NoteRepository extends CrudRepository<Note, Long> {
 
-    List<Note> findAll();
+    List<Note> findAll(Sort sort);
 
 }
