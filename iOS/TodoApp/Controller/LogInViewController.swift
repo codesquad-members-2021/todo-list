245c716266c3f6886ba21f232a4db70942d501ec
Ray suh <readingod@naver.com>
2021-04-16 13:42:09 +0900

    Feat: ID & Password Textfield conection And Call NetworkManager.loginPost

--- iOS/TodoApp/Controller/LogInViewController.swift
+++ iOS/TodoApp/Controller/LogInViewController.swift
@@ -3,8 +3,15 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        passwordTextField.isSecureTextEntry = true
     }
 
+    @IBAction func logInButtonTouched(_ sender: Any) {
+        NetworkManager.loginPost(id: idTextField.text ?? "", password: passwordTextField.text ?? "")
+    }
 }
