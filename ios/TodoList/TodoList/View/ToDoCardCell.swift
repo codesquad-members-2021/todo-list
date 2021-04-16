dbdd8b3029a60ebb20f226f88714515105fe9ae5
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-04-16 11:16:30 +0900

    Layout touch

--- ios/TodoList/TodoList/View/ToDoCardCell.swift
+++ ios/TodoList/TodoList/View/ToDoCardCell.swift
@@ -15,7 +15,7 @@ class ToDoCardCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         self.backgroundColor = .clear
-        self.contentView.layer.cornerRadius = 30
+        self.contentView.layer.cornerRadius = 20
         self.contentView.layer.masksToBounds = true
         self.contentView.layer.borderWidth = 2
         self.contentView.layer.borderColor = UIColor.white.cgColor
@@ -28,4 +28,11 @@ class ToDoCardCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 0))
+    }
+    
 }
