6ac3ab0c00b48ba90d9d1583b5faa40a88ee7aab
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-04-17 20:52:36 +0900

    refactor : 겹치는 에러메세지 제거

--- be/todo/src/main/java/com/codesquad/todo/web/controller/ExceptionController.java
+++ be/todo/src/main/java/com/codesquad/todo/web/controller/ExceptionController.java
@@ -17,27 +17,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ExceptionController {
     private static final Logger logger = LoggerFactory.getLogger(ExceptionController.class);
 
-    private static final String ERROR_MESSAGE_NOT_FOUND = "리소스를 찾지 못했습니다!";
-    private static final String ERROR_MESSAGE_LOG_ACTION_COMMAND_NOT_FOUND = "발생한 작업에 해당하는 로그 액션 커맨드를 찾지 못했습니다!";
-    private static final String ERROR_MESSAGE_INVALID_ENTITY_DETECTED = "잘못된 엔티티가 발견되었습니다! 구성 문자열이 비어있습니다!";
-
     @ExceptionHandler({UserNotFoundException.class, TaskNotFoundException.class, ColumnNotFoundException.class})
     public ResponseEntity<ErrorDto> handlerNotFoundException(RuntimeException exception) {
-        logger.error(ERROR_MESSAGE_NOT_FOUND);
         logger.error(exception.getMessage());
         return createErrorResponse(exception);
     }
 
     @ExceptionHandler(IllegalActionCommandException.class)
     public ResponseEntity<ErrorDto> handlerIllegalActionCommandException(IllegalActionCommandException exception) {
-        logger.error(ERROR_MESSAGE_LOG_ACTION_COMMAND_NOT_FOUND);
         logger.error(exception.getMessage());
         return createErrorResponse(exception);
     }
 
     @ExceptionHandler(BindException.class)
     public ResponseEntity<ErrorDto> handlerInvalidEntityDetectedException(BindException exception) {
-        logger.error(ERROR_MESSAGE_INVALID_ENTITY_DETECTED);
         logger.error(exception.getMessage());
         return createErrorResponse(exception);
     }
