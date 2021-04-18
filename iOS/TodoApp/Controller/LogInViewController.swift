7b07c0bd700e9296f31e335dde3f8a3d125a5c91
Ji-Sun Kim <jisunkim98@gmail.com>
2021-04-18 14:55:58 +0900

    #32 Refactor: code refactoring

--- iOS/TodoApp/Controller/LogInViewController.swift
+++ iOS/TodoApp/Controller/LogInViewController.swift
@@ -3,18 +3,30 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
+    @IBOutlet weak var logInMainView: UIView!
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var logInImageView: UIImageView!
     @IBOutlet weak var logInImageView2: UIImageView!
     
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var pwLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        logInMainView.backgroundColor = UIColor(patternImage: UIImage(named: "띵언")!)
+        logInMainView.layer.cornerRadius = 5
         passwordTextField.isSecureTextEntry = true
         logInImageView.layer.masksToBounds = true
         logInImageView.layer.cornerRadius = logInImageView.frame.width/3
         logInImageView2.layer.masksToBounds = true
         logInImageView2.layer.cornerRadius = logInImageView2.frame.width/3
+        idLabel.layer.masksToBounds = true
+        pwLabel.layer.masksToBounds = true
+        idLabel.layer.cornerRadius = 5
+        pwLabel.layer.cornerRadius = 5
+        idTextField.attributedPlaceholder = NSAttributedString(string: "아이디를 입력하세요~", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "비밀번호를 입력하세요~", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
+        
     }
 
     @IBAction func logInButtonTouched(_ sender: Any) {
